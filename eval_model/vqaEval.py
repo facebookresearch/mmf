778c3a63916ad64bbdbc91eeacc59cbe3264d338
Vivek Natarajan <nvivek@devfair0183.h2.fair>
2018-07-20 16:43:07 -0700

    lint

--- eval_model/vqaEval.py
+++ eval_model/vqaEval.py
@@ -1,3 +1,10 @@
+# coding=utf-8
+
+__author__ = 'aagrawal'
+
+# This code is based on the code written by Tsung-Yi Lin for MSCOCO Python API available at the following link:
+# (https://github.com/tylin/coco-caption/blob/master/pycocoevalcap/eval.py).
+
 import sys
 import re
 
--- top_down_bottom_up/unittests.py
+++ top_down_bottom_up/unittests.py
@@ -10,11 +10,11 @@ import unittest
 import torch
 from torch.autograd import Variable
 from global_variables.global_variables import use_cuda
-from top_down_bottom_up.nonlinear_layer import nonlinear_layer
 from top_down_bottom_up.question_embeding import QuestionEmbeding
 from top_down_bottom_up.image_embedding import image_embedding
 from top_down_bottom_up.classifier import logit_classifier
-from top_down_bottom_up.top_down_bottom_up_model import top_down_bottom_up_model
+from top_down_bottom_up.top_down_bottom_up_model \
+    import top_down_bottom_up_model
 import numpy as np
 
 
@@ -26,8 +26,12 @@ class Test_top_down_bottom_up_model(unittest.TestCase):
         text_embeding_dim = 64
         image_embedding_dim = 32
 
-        my_classifier = logit_classifier(joint_embedding_dim, num_ans_candidates, image_embedding_dim, text_embeding_dim)
-        joint_embedding = Variable(torch.randn(batch_size,joint_embedding_dim))
+        my_classifier = logit_classifier(joint_embedding_dim,
+                                         num_ans_candidates,
+                                         image_embedding_dim,
+                                         text_embeding_dim)
+        joint_embedding = Variable(torch.randn(batch_size,
+                                   joint_embedding_dim))
         res = my_classifier(joint_embedding)
         self.assertEqual((12, 20), res.shape)
 
@@ -38,8 +42,12 @@ class Test_top_down_bottom_up_model(unittest.TestCase):
         text_embeding_dim = 64
         image_embedding_dim = 32
 
-        my_classifier = logit_classifier(joint_embedding_dim, num_ans_candidates, image_embedding_dim, text_embeding_dim)
-        joint_embedding = Variable(torch.randn(batch_size,joint_embedding_dim))
+        my_classifier = logit_classifier(joint_embedding_dim,
+                                         num_ans_candidates,
+                                         image_embedding_dim,
+                                         text_embeding_dim)
+        joint_embedding = Variable(torch.randn(batch_size,
+                                   joint_embedding_dim))
         res = my_classifier(joint_embedding)
         self.assertEqual((1, 20), res.shape)
 
@@ -52,9 +60,16 @@ class Test_top_down_bottom_up_model(unittest.TestCase):
         batch_first = True
         batch_size = 32
         question_len = 10
-        my_word_embedding_model = QuestionEmbeding(num_vocab, embedding_dim, lstm_dim, lstm_layer, dropout, batch_first)
-        my_word_embedding_model = my_word_embedding_model.cuda() if use_cuda else my_word_embedding_model
-        input_txt = Variable(torch.rand(batch_size, question_len).type(torch.LongTensor) %num_vocab)
+        my_word_embedding_model = QuestionEmbeding(num_vocab,
+                                                   embedding_dim,
+                                                   lstm_dim,
+                                                   lstm_layer,
+                                                   dropout,
+                                                   batch_first)
+        my_word_embedding_model = (my_word_embedding_model.cuda()
+                                   if use_cuda else my_word_embedding_model)
+        input_txt = Variable(torch.rand(batch_size,
+                             question_len).type(torch.LongTensor) % num_vocab)
         input_txt = input_txt.cuda() if use_cuda else input_txt
         embedding = my_word_embedding_model(input_txt, batch_first=True)
         self.assertEqual((32, 512), embedding.shape)
@@ -65,11 +80,18 @@ class Test_top_down_bottom_up_model(unittest.TestCase):
         hidden_size = 30
         num_of_loc = 5
         batch_size = 16
-        my_image_embeding = image_embedding(image_feat_dim, txt_embedding_dim, hidden_size)
-        image_feat = Variable(torch.randn(batch_size,num_of_loc,image_feat_dim))
-        txt = Variable(torch.randn(batch_size,txt_embedding_dim))
+        my_image_embeding = image_embedding(image_feat_dim,
+                                            txt_embedding_dim,
+                                            hidden_size)
+        image_feat = Variable(torch.randn(batch_size,
+                              num_of_loc,
+                              image_feat_dim))
+        txt = Variable(torch.randn(batch_size,
+                       txt_embedding_dim))
         res = my_image_embeding(image_feat, txt)
-        self.assertEqual((batch_size,image_feat_dim), res.shape)
+        self.assertEqual((batch_size,
+                         image_feat_dim),
+                         res.shape)
 
     def test_model(self):
         image_feat_dim = 40
@@ -79,24 +101,35 @@ class Test_top_down_bottom_up_model(unittest.TestCase):
         num_of_loc = 5
         batch_size = 16
         num_vocab = 60
-        num_ans_candidates=35
-        joint_embedding_dim=500
+        num_ans_candidates = 35
+        joint_embedding_dim = 500
         question_len = 13
         batch_first = True
-        image_embedding_model = image_embedding(image_feat_dim, lstm_dim,hidden_size)
-        question_embedding_model = QuestionEmbeding(num_vocab, txt_embedding_dim, lstm_dim,
-                                                    lstm_layer=2, dropout=0.1, batch_first=batch_first)
-        my_classifier = logit_classifier(joint_embedding_dim, num_ans_candidates, image_feat_dim,
+        image_embedding_model = image_embedding(image_feat_dim,
+                                                lstm_dim,
+                                                hidden_size)
+        question_embedding_model = QuestionEmbeding(num_vocab,
+                                                    txt_embedding_dim,
+                                                    lstm_dim,
+                                                    lstm_layer=2,
+                                                    dropout=0.1,
+                                                    batch_first=batch_first)
+        my_classifier = logit_classifier(joint_embedding_dim,
+                                         num_ans_candidates,
+                                         image_feat_dim,
                                          txt_embedding_dim)
         loss = torch.nn.CrossEntropyLoss()
 
-        my_model = top_down_bottom_up_model(image_embedding_model, question_embedding_model, my_classifier,loss)
-        #image_feat = Variable(torch.randn(batch_size, num_of_loc, image_feat_dim))
-        image_feat = np.random.rand(batch_size, num_of_loc, image_feat_dim)
-        input_txt = Variable(torch.rand(batch_size, question_len).type(torch.LongTensor) % num_vocab)
+        my_model = top_down_bottom_up_model(image_embedding_model,
+                                            question_embedding_model,
+                                            my_classifier, loss)
+        image_feat = np.random.rand(batch_size,
+                                    num_of_loc,
+                                    image_feat_dim)
+        input_txt = Variable(torch.rand(batch_size,
+                             question_len).type(torch.LongTensor) % num_vocab)
         res = my_model(image_feat, input_txt, batch_first)
-        self.assertEqual((batch_size, num_ans_candidates),res.shape)
-
+        self.assertEqual((batch_size, num_ans_candidates), res.shape)
 
 
 if __name__ == '__main__':
