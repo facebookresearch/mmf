24d46ed8a4afff5bfe071cb6d8b2758bc47ab7d5
Vivek Natarajan <nvivek@devfair0183.h2.fair>
2018-07-20 16:11:47 -0700

    lint

--- top_down_bottom_up/intermediate_layer.py
+++ top_down_bottom_up/intermediate_layer.py
@@ -8,10 +8,11 @@
 
 import torch.nn as nn
 
+
 class inter_layer(nn.Module):
     def __init__(self, dim, n_layer):
         super(inter_layer, self).__init__()
-        layers=[]
+        layers = []
         for i in range(n_layer):
             layers.append(nn.Linear(dim, dim))
             layers.append(nn.ReLU())
