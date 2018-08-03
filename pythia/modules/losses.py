4536649d999d938743ac08d58e38b15455dd1fbd
Amanpreet Singh <asg@fb.com>
2018-08-03 13:08:13 -0700

    Refactor out losses module

---
+++ pythia/modules/losses.py
@@ -0,0 +1,125 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Loss(nn.Module):
+    def __init__(self, loss_type):
+        super(Loss, self).__init__()
+        if loss_type == 'logit_bce':
+            self.loss_criterion = LogitBinaryCrossEntropy()
+        elif loss_type == 'softmax_kl':
+            self.loss_criterion = SoftmaxKlDivLoss()
+        elif loss_type == 'wrong':
+            self.loss_criterion = WrongLoss()
+        elif loss_type == 'combined':
+            self.loss_criterion = CombinedLoss()
+        else:
+            raise NotImplementedError("Unknown loss type: %s" % loss_type)
+
+    def forward(self, *args, **kwargs):
+        return self.loss_criterion(*args, **kwargs)
+
+
+class LogitBinaryCrossEntropy(nn.Module):
+    def __init__(self):
+        super(LogitBinaryCrossEntropy, self).__init__()
+
+    def forward(self, pred_score, target_score, weights=None):
+        loss = F.binary_cross_entropy_with_logits(pred_score,
+                                                  target_score,
+                                                  size_average=True)
+        loss = loss * target_score.size(1)
+        return loss
+
+
+def kl_div(log_x, y):
+    y_is_0 = torch.eq(y.data, 0)
+    y.data.masked_fill_(y_is_0, 1)
+    log_y = torch.log(y)
+    y.data.masked_fill_(y_is_0, 0)
+    res = y * (log_y - log_x)
+
+    return torch.sum(res, dim=1, keepdim=True)
+
+
+class WeightedSoftmaxLoss(nn.Module):
+    def __init__(self):
+        super(WeightedSoftmaxLoss, self).__init__()
+
+    def forward(self, pred_score, target_score):
+        tar_sum = torch.sum(target_score, dim=1, keepdim=True)
+        tar_sum_is_0 = torch.eq(tar_sum, 0)
+        tar_sum.masked_fill_(tar_sum_is_0, 1.0e-06)
+        tar = target_score / tar_sum
+
+        res = F.log_softmax(pred_score, dim=1)
+        loss = kl_div(res, tar)
+        loss = loss * tar_sum
+        loss = torch.sum(loss) / loss.size(0)
+        return loss
+
+
+class SoftmaxKlDivLoss(nn.Module):
+    def __init__(self):
+        super(SoftmaxKlDivLoss, self).__init__()
+
+    def forward(self, pred_score, target_score):
+        tar_sum = torch.sum(target_score, dim=1, keepdim=True)
+        tar_sum_is_0 = torch.eq(tar_sum, 0)
+        tar_sum.masked_fill_(tar_sum_is_0, 1.0e-06)
+        tar = target_score / tar_sum
+
+        res = F.log_softmax(pred_score, dim=1)
+        loss = kl_div(res, tar)
+        loss = torch.sum(loss) / loss.size(0)
+        return loss
+
+
+class WrongLoss(nn.Module):
+    def __init__(self):
+        super(WrongLoss, self).__init__()
+
+    def forward(self, pred_score, target_score):
+        tar_sum = torch.sum(target_score, dim=1, keepdim=True)
+        tar_sum_is_0 = torch.eq(tar_sum, 0)
+        tar_sum.masked_fill_(tar_sum_is_0, 1.0e-06)
+        tar = target_score / tar_sum
+
+        res = F.log_softmax(pred_score, dim=1)
+        loss = F.kl_div(res, tar, size_average=True)
+        loss *= target_score.size(1)
+        return loss
+
+
+class CombinedLoss(nn.Module):
+    def __init__(self, weight_softmax):
+        super(CombinedLoss, self).__init__()
+        self.weight_softmax = weight_softmax
+
+    def forward(self, pred_score, target_score):
+        tar_sum = torch.sum(target_score, dim=1, keepdim=True)
+        tar_sum_is_0 = torch.eq(tar_sum, 0)
+        tar_sum.masked_fill_(tar_sum_is_0, 1.0e-06)
+        tar = target_score / tar_sum
+
+        res = F.log_softmax(pred_score, dim=1)
+        loss1 = kl_div(res, tar)
+        loss1 = torch.sum(loss1) / loss1.size(0)
+
+        loss2 = F.binary_cross_entropy_with_logits(pred_score,
+                                                   target_score,
+                                                   size_average=True)
+        loss2 *= target_score.size(1)
+
+        loss = self.weight_softmax * loss1 + loss2
+
+        return loss
