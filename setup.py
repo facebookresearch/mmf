03339d3e329d5aa573f8058566129f2a4a3bfdca
Amanpreet Singh <asg@fb.com>
2018-08-06 13:15:49 -0700

    Add requirements and setup files

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='pythia',
+      version='0.2',
+      # And any other dependencies foo needs
+      install_requires=['torch', 'numpy', 'torchvision', 'tensorboardX'])
