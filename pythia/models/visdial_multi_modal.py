0bfd9490b0f26c9c1da92a9f2236a81201548419
Amanpreet Singh <apsdehal@gmail.com>
2018-08-17 17:26:33 -0700

    Adapt visdial multi modal for changes

--- pythia/models/visdial_multi_modal.py
+++ pythia/models/visdial_multi_modal.py
@@ -9,7 +9,7 @@ class VisDialMultiModalModel(VQAMultiModalModel):
         super(VisDialMultiModalModel, self).__init__(config)
 
     def build(self):
-        self._init_question_embedding()
+        self._init_text_embedding()
         self._init_image_encoders()
         self._init_image_embeddings()
         self._init_combine_layer()
@@ -19,7 +19,7 @@ class VisDialMultiModalModel(VQAMultiModalModel):
     def get_optimizer_parameters(self, config):
         # TODO: Update after implementing decoder
         params = [{'params': self.img_embeddings_list.parameters()},
-                  {'params': self.question_embeddings.parameters()},
+                  {'params': self.text_embeddings.parameters()},
                   {'params': self.multi_modal_combine_layer.parameters()},
                   {'params': self.decoder.projection_layer.parameters()},
                   {'params': self.img_feat_encoders.parameters(),
@@ -28,15 +28,15 @@ class VisDialMultiModalModel(VQAMultiModalModel):
 
         return params
 
-    def _update_question_embedding_args(self, args):
+    def _update_text_embedding_args(self, args):
         parent = super(VisDialMultiModalModel, self)
-        parent._update_question_embedding_args(args)
+        parent._update_text_embedding_args(args)
         # Add embedding vectors to args
         args['embedding_vectors'] = self.config['embedding_vectors']
 
     def _init_decoder(self):
-        embedding = self.question_embeddings[0].module
-        embedding_dim = self.question_embeddings[0].embedding_dim
+        embedding = self.text_embeddings[0].module
+        embedding_dim = self.text_embeddings[0].embedding_dim
         hidden_dim = self.multi_modal_combine_layer.out_dim
 
         self.decoder = VisDialDiscriminator({
@@ -44,17 +44,23 @@ class VisDialMultiModalModel(VQAMultiModalModel):
             'hidden_dim': hidden_dim,
         }, embedding)
 
+    def combine_embeddings(self, *args):
+        return self.multi_modal_combine_layer(*args)
+
+    def calculate_logits(self, joint_embedding, **kwargs):
+        return self.decoder(joint_embedding, **kwargs)
+
     def forward(self,
-                questions,
+                texts,
                 answer_options,
                 histories,
                 image_features,
                 image_dims,
                 **kwargs):
-            # TODO: Move flattening to separate function
-            question_embeddings = []
 
-            questions = questions.view(-1, questions.size(2))
+            texts = texts.view(-1, texts.size(2))
+            text_embedding_total = self.process_text_embedding(texts)
+            histories_total = self.process_text_embedding(histories)
 
             for idx, image_feature in enumerate(image_features):
                 feature_size = image_feature.size()[2:]
@@ -63,39 +69,28 @@ class VisDialMultiModalModel(VQAMultiModalModel):
             size = image_dims.size()[2:]
             image_dims = image_dims.view(-1, *size)
 
-            for q_model in self.question_embeddings:
-                q_embedding = q_model(questions)
-                question_embeddings.append(q_embedding)
-            question_embedding_total = torch.cat(question_embeddings, dim=1)
-
             assert (len(image_features) ==
                     len(self.img_feat_encoders)), \
                 "number of image feature model doesnot equal \
                  to number of image features"
 
-            image_embeddings = []
-
-            for i, image_feat_variable in enumerate(image_features):
-                image_dim_variable_use = None if i > 0 else image_dims
-                image_feat_variable_ft = (
-                    self.img_feat_encoders[i](image_feat_variable))
-
-                image_embedding_models_i = self.img_embeddings_list[i]
-                for i_model in image_embedding_models_i:
-                    i_embedding = i_model(
-                        image_feat_variable_ft,
-                        question_embedding_total, image_dim_variable_use)
-                    image_embeddings.append(i_embedding)
-
-            image_embedding_total = torch.cat(image_embeddings, dim=1)
+            image_embedding_total = self.process_image_embedding(
+                image_features,
+                image_dims,
+                text_embedding_total
+            )
 
             if self.inter_model is not None:
                 image_embedding_total = self.inter_model(image_embedding_total)
 
-            joint_embedding = self.multi_modal_combine_layer(
-                image_embedding_total, question_embedding_total)
-            logit_res = self.decoder(joint_embedding, {
+            joint_embedding = self.combine_embeddings(
+                image_embedding_total,
+                text_embedding_total,
+                histories_total
+            )
+
+            kwargs = {
                 'answer_options': answer_options,
                 'answer_options_len': kwargs['answer_options_len']
-            })
-            return logit_res
+            }
+            return self.calculate_logits(joint_embedding, **kwargs)
