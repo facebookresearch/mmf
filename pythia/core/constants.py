4fb6c68b66cc6e83c2ca42b5ddaab61b98c58d71
Amanpreet Singh <apsdehal@gmail.com>
2018-10-17 22:53:53 -0700

    Update trainer to adapt to multi-task changes

--- pythia/core/constants.py
+++ pythia/core/constants.py
@@ -1,7 +1,1 @@
-# Mappings of task name to their respective arguments in command line
-task_name_mapping = {
-    'vqa2': 'VQA2Task',
-    'visdial': 'VisualDialogTask'
-}
-
 imdb_version = 1
--- pythia/core/trainer.py
+++ pythia/core/trainer.py
@@ -10,12 +10,12 @@ from pythia.utils.configuration import Configuration
 from pythia.utils.checkpoint import Checkpoint
 from pythia.utils.logger import Logger
 from pythia.utils.general import lr_lambda_update, clip_gradients, \
-                get_optimizer_parameters
+                get_optimizer_parameters, dict_to_string
 from pythia.utils.build import build_model
 from pythia.utils.timer import Timer
 from pythia.utils.early_stopping import EarlyStopping
 from pythia.core.task_loader import TaskLoader
-from pythia.modules.losses import Loss
+from pythia.core.registry import Registry
 
 
 class Trainer:
@@ -28,6 +28,7 @@ class Trainer:
         self.load_task()
 
         self.writer = Logger(self.config)
+        Registry.register('writer', self.writer)
 
         self.load_model()
         self.load_optimizer()
@@ -40,12 +41,18 @@ class Trainer:
         self.configuration.update_with_args(self.args, force=True)
 
         self.config = self.configuration.get_config()
+        Registry.register('config', self.config)
 
         self.config_based_setup()
 
     def load_task(self):
         self.task_loader = TaskLoader(self.config)
-        self.configuration.update_with_task_config(self.task_loader)
+        self.task_loader.load_task()
+        self.task_loader.load_config()
+
+        self.configuration.update_with_task_config(
+            self.task_loader.get_config()
+        )
 
         # Update with the config override if passed
         self.configuration.override_with_cmd_config(self.args.config_override)
@@ -54,16 +61,14 @@ class Trainer:
         self.configuration.update_with_args(self.args)
         self.configuration.pretty_print()
 
-        self.task_loader.load_dataset()
         self.task_loader.make_dataloaders()
 
         self.train_loader = self.task_loader.train_loader
         self.dev_loader = self.task_loader.dev_loader
         self.test_loader = self.task_loader.test_loader
-
-        self.train_meter = self.task_loader.train_meter
-        self.dev_meter = self.task_loader.dev_meter
-        self.test_meter = self.task_loader.test_meter
+        self.train_task = self.task_loader.train_task
+        self.dev_task = self.task_loader.dev_task
+        self.test_task = self.task_loader.test_task
 
     def load_model(self):
         attributes = self.config['model_attributes'][self.config['model']]
@@ -91,14 +96,15 @@ class Trainer:
         self.optimizer = optimizer_class(parameters, **rest_optimizer_params)
 
     def load_extras(self):
-        self.criterion = Loss(self.config['loss'])
-
         self.checkpoint = Checkpoint(self)
 
-        monitored_metric = self.config['task_attributes']['monitored_metric']
-        metric_minimize = self.config['task_attributes']['metric_minimize']
-        should_early_stop = self.config['task_attributes']['should_early_stop']
-        patience = self.config['training_parameters']['patience']
+        training_parameters = self.config['training_parameters']
+
+        monitored_metric = training_parameters['monitored_metric']
+        metric_minimize = training_parameters['metric_minimize']
+        should_early_stop = training_parameters['should_early_stop']
+        patience = training_parameters['patience']
+
         self.early_stopping = EarlyStopping(self.model,
                                             self.checkpoint,
                                             self.dev_meter,
@@ -146,11 +152,14 @@ class Trainer:
 
         while self.current_iteration < max_iterations:
             self.current_epoch += 1
+            Registry.register('current_epoch', self.current_epoch)
+
             if should_check_on_epoch and self.current_epoch > max_epochs:
                 break
 
             for batch in self.train_loader:
                 self.current_iteration += 1
+                Registry.register('current_iteration', self.current_iteration)
 
                 if self.current_iteration > max_iterations:
                     break
@@ -164,7 +173,7 @@ class Trainer:
 
                 # Arguments should be a dict at this point
                 output = self.model(**data)
-                loss = self.criterion(output, y)
+                loss = self.task_loader.calculate_loss('train', output, y)
 
                 loss.backward()
 
@@ -173,7 +182,6 @@ class Trainer:
                                    self.writer, self.config)
 
                 self.optimizer.step()
-                self.train_meter(output, y)
 
                 extra_info = None
                 should_print = self.current_iteration % log_interval == 0
@@ -181,17 +189,13 @@ class Trainer:
                 # TODO: Move in separate function
                 if should_print is True:
                     sys.stdout.flush()
-                    extra_info = self.single_batch_eval(self.dev_loader,
-                                                        self.dev_meter)
+                    extra_info = self.single_batch_eval('dev', self.dev_loader)
                     time_taken = self.train_timer.get_time_since_start()
                     extra_info += ", time: %s" % time_taken
 
                 # Don't print train metrics if it is not log interval
                 # so as to escape clutter
-                self.task_loader.report_metrics(self.writer,
-                                                self.train_meter,
-                                                loss.data.item(),
-                                                self.current_iteration,
+                self.task_loader.report_metrics('train', loss.data.item(),
                                                 extra_info=extra_info,
                                                 should_print=should_print)
 
@@ -201,8 +205,7 @@ class Trainer:
                 if self.current_iteration % snapshot_interval == 0:
                     # TODO: Implement early stopping
                     # TODO: Do validation check here
-                    avg_loss = self.evaluate(self.dev_loader,
-                                             self.dev_meter)
+                    avg_loss = self.evaluate('dev', self.dev_loader)
                     time_taken = self.snapshot_timer.get_time_since_start()
                     extra_info = ", time: %s" % time_taken
                     self.snapshot_timer.reset()
@@ -210,10 +213,7 @@ class Trainer:
                                                avg_loss)
                     extra_info += "\n %s" % self.early_stopping.get_info()
 
-                    self.task_loader.report_metrics(self.writer,
-                                                    self.dev_meter,
-                                                    avg_loss,
-                                                    self.current_iteration,
+                    self.task_loader.report_metrics('dev', avg_loss,
                                                     extra_info=extra_info)
 
                     if stop is True:
@@ -222,39 +222,38 @@ class Trainer:
 
                 gc.collect()
 
-        avg_test_loss = self.evaluate(self.test_loader, self.test_meter)
-        self.task_loader.report_metrics(self.writer, self.test_meter,
-                                        avg_test_loss, self.current_iteration)
+        avg_test_loss = self.evaluate('test', self.test_loader)
+        self.task_loader.report_metrics('test', avg_test_loss)
 
-    def single_batch_eval(self, loader, meter):
+    def single_batch_eval(self, dataset_type, loader):
         self.model.eval()
         batch = next(iter(loader))
-        meter.reset()
+        self.task_loader.reset_meters(dataset_type)
+
         data, y = self.task_loader.prepare_batch(loader.dataset_type, batch)
         output = self.model(**data)
-        meter(output, y)
-        loss = self.criterion(output, y)
+        self.task_loader.calculate_loss(dataset_type, output, y)
         self.model.train()
 
         # TODO: Do replace in log string function itself
-        return meter.get_log_string(loss).replace("Average", "Single")
+        return "Dev: " + dict_to_string(Registry.get('metrics.%s' %
+                                                     dataset_type))
 
-    def evaluate(self, loader, meter):
+    def evaluate(self, dataset_type, loader):
         self.model.eval()
+        self.task_loader.reset_meters(dataset_type)
 
-        meter.reset()
         total_loss = 0
         total_samples = 0
 
         for batch in loader:
-            data, y = self.task_loader.prepare_batch(loader.dataset_type,
+            data, y = self.task_loader.prepare_batch(dataset_type,
                                                      batch)
             total_samples += y.size(0)
 
             output = self.model(**data)
 
-            meter(output, y)
-            loss = self.criterion(output, y)
+            loss = self.task_loader.calculate_loss(dataset_type, output, y)
             if loss is not None:
                 total_loss += loss.data.item() * y.size(0)
 
