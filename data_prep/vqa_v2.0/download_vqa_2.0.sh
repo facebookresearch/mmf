18cd8b9c23db229d8e0e55b0a6c2d337cb66d2e3
meetshah1995 <meetshah1995@gmail.com>
2019-01-06 22:44:25 -0800

    Update download paths

--- data_prep/vqa_v2.0/download_vqa_2.0.sh
+++ data_prep/vqa_v2.0/download_vqa_2.0.sh
@@ -9,19 +9,19 @@ rm glove.6B.zip
 
 ##VQA2.0
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Annotations_Val_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Train_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Test_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Complementary_Pairs_Train_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Train_mscoco.zip
 
-wget http://visualqa.org/data/mscoco/vqa/v2_Complementary_Pairs_Val_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Val_mscoco.zip
 
 unzip v2_Annotations_Train_mscoco.zip
 rm v2_Annotations_Train_mscoco.zip
@@ -48,10 +48,3 @@ rm v2_Complementary_Pairs_Val_mscoco.zip
 ### get minival and val2train
 wget https://s3-us-west-1.amazonaws.com/vqa-suite/val_split/v2_OpenEnded_mscoco_minival2014_questions.json
 wget https://s3-us-west-1.amazonaws.com/vqa-suite/val_split/v2_OpenEnded_mscoco_val2train2014_questions.json
-
-
-### Image Features
-
-
-
-
