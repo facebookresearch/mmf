46acce8d996f6c6c820222561ae9ac5ef58b4d78
Amanpreet Singh <apsdehal@gmail.com>
2019-04-10 15:35:14 -0700

    Add new options to config like pin_memory and updates to concat dataset
    - For better accessibility, concat dataset now allow calling attributes
    of the child dataset

--- pythia/tasks/concat_dataset.py
+++ pythia/tasks/concat_dataset.py
@@ -5,6 +5,8 @@ from torch.utils.data import ConcatDataset
 
 
 class PythiaConcatDataset(ConcatDataset):
+    # This functions should only be called once even if they return nothing
+    _SINGLE_CALL_FUNCS = []
     def __init__(self, datasets):
         super().__init__(datasets)
         self._dir_representation = dir(self)
@@ -22,10 +24,17 @@ class PythiaConcatDataset(ConcatDataset):
         else:
             raise AttributeError(name)
 
+    def _get_single_call_funcs(self):
+        return PythiaConcatDataset._SINGLE_CALL_FUNCS
 
     def _call_all_datasets_func(self, name, *args, **kwargs):
         for dataset in self.datasets:
             value = getattr(dataset, name)(*args, **kwargs)
             if value is not None:
-                raise RuntimeError("Functions returning values can't be "
-                                   "called through PythiaConcatDataset")
+                # TODO: Log a warning here
+                return value
+                # raise RuntimeError("Functions returning values can't be "
+                #                    "called through PythiaConcatDataset")
+            if hasattr(dataset, 'get_single_call_funcs') \
+                and name in dataset.get_single_call_funcs():
+                return
