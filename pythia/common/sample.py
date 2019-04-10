4a39eed7226df71546b4cfe224cbd384722f310d
Amanpreet Singh <apsdehal@gmail.com>
2019-04-10 15:18:16 -0700

    Allow more options (tuple, dict) for SampleList init
    - Also, allow non-blocking .to
    - Fix type when the type is not Sample for the list passed

--- pythia/common/sample.py
+++ pythia/common/sample.py
@@ -1,4 +1,5 @@
 import torch
+import collections
 
 from collections import OrderedDict
 from copy import deepcopy
@@ -19,10 +20,19 @@ class Sample(OrderedDict):
 
 
 class SampleList(OrderedDict):
+    _TENSOR_FIELD_ = "_tensor_field"
+
     def __init__(self, samples=[]):
         super().__init__(self)
 
         if len(samples) == 0:
+            return
+
+        if self._check_and_load_dict(samples):
+            return
+        # If passed sample list was in form of key, value pairs of tuples
+        # return after loading these
+        if self._check_and_load_tuple(samples):
             return
 
         fields = samples[0].keys()
@@ -31,29 +41,63 @@ class SampleList(OrderedDict):
             if isinstance(samples[0][field], torch.Tensor):
                 size = (len(samples), *samples[0][field].size())
                 self[field] = samples[0][field].new_empty(size)
+                if self._get_tensor_field() is None:
+                    self._set_tensor_field(field)
             else:
                 self[field] = [None for _ in range(len(samples))]
 
             for idx, sample in enumerate(samples):
-                if len(sample[field]) != len(samples[0][field]):
-                    raise RuntimeError("Fields for all samples must be"
-                                       " equally sized")
+                # it should be a tensor but not a 0-d tensor
+                if isinstance(sample[field], torch.Tensor) and \
+                    len(sample[field].size()) != 0 and \
+                    sample[field].size(0) != samples[0][field].size(0):
+                    raise AssertionError("Fields for all samples must be"
+                                         " equally sized.")
+
                 self[field][idx] = self._get_data_copy(sample[field])
+
+            if isinstance(samples[0][field], collections.Mapping):
+                self[field] = SampleList(self[field])
+
+    def _check_and_load_tuple(self, samples):
+        if isinstance(samples[0], (tuple, list)) \
+            and isinstance(samples[0][0], str):
+            for kv_pair in samples:
+                self.add_field(kv_pair[0], kv_pair[1])
+            return True
+        else:
+            return False
+
+    def _check_and_load_dict(self, samples):
+        if isinstance(samples, collections.Mapping):
+            for key, value in samples.items():
+                self.add_field(key, value)
+            return True
+        else:
+            return False
+
+    def _fix_sample_type(self, samples):
+        if not isinstance(samples[0], Sample):
+            proper_samples = []
+            for sample in samples:
+                proper_samples.append(Sample(sample))
+            samples = proper_samples
+        return samples
 
     def __getattr__(self, key):
         if key not in self:
-            raise AttributeError("Key {} not found in the list."
+            raise AttributeError("Key {} not found in the SampleList. "
                                  "Valid choices are {}"
                                  .format(key, self.fields()))
         fields = self.keys()
 
-        if item in fields:
-            return self[item]
+        if key in fields:
+            return self[key]
 
         sample = Sample()
 
         for field in fields:
-            sample[field] = self[field][item]
+            sample[field] = self[field][key]
 
         return sample
 
@@ -75,39 +119,77 @@ class SampleList(OrderedDict):
     def fields(self):
         return list(self.keys())
 
+    def get_fields(self, fields):
+        current_fields = self.fields()
+
+        return_list = SampleList()
+
+        for field in fields:
+            if field not in current_fields:
+                raise AttributeError("{} not present in SampleList. "
+                                     "Valid choices are {}"
+                                     .format(field, current_fields))
+            return_list.add_field(field, self[field])
+
+        return return_list
+
+    def get_field(self, field):
+        return self[field]
+
     def _get_data_copy(self, data):
         if isinstance(data, torch.Tensor):
-            copy_ = data.new_tensor(data)
+            copy_ = data.clone()
         else:
             copy_ = deepcopy(data)
         return copy_
 
+    def _get_tensor_field(self):
+        return self.__dict__.get(SampleList._TENSOR_FIELD_, None)
+
+    def _set_tensor_field(self, value):
+        self.__dict__[SampleList._TENSOR_FIELD_] = value
+
+    def get_batch_size(self):
+        tensor_field = self._get_tensor_field()
+        assert tensor_field is not None, "There is no tensor yet in SampleList"
+
+        return self[tensor_field].size(0)
+
     def add_field(self, field, data):
         fields = self.fields()
+        tensor_field = self._get_tensor_field()
 
         if len(fields) == 0:
             self[field] = self._get_data_copy(data)
         else:
-            if len(data) != len(self[fields[0]]):
-                raise RuntimeError("Field to be added must have same size "
-                                   "as existing fields in SampleList."
-                                   "Passed size: {}, Required size: {}"
-                                   .format(len(data), len(self[fields[0]])))
+            if isinstance(data, torch.Tensor) and \
+                len(data.size()) != 0 and \
+                tensor_field is not None and \
+                data.size(0) != self[tensor_field].size(0):
+                raise AssertionError("A tensor field to be added must "
+                                     "have same size as existing tensor "
+                                     "fields in SampleList. "
+                                     "Passed size: {}, Required size: {}"
+                                     .format(len(data), len(self[fields[0]])))
             self[field] = self._get_data_copy(data)
 
-    def to(self, device):
+        if isinstance(self[field], torch.Tensor) and tensor_field is None:
+            self._set_tensor_field(field)
+
+    def to(self, device, non_blocking=True):
         fields = self.keys()
         sample_list = self.copy()
         if not isinstance(device, torch.device):
             if not isinstance(device, str):
-                raise TypeError("device must be either 'str' or"
+                raise TypeError("device must be either 'str' or "
                                 "'torch.device' type, {} found"
                                 .format(type(device)))
             device = torch.device(device)
 
         for field in fields:
-            if hasattr(sample_list[field], "to") \
-               and sample_list[field].device != device:
-                sample_list[field] = sample_list[field].to(device)
+            if hasattr(sample_list[field], "to"):
+                sample_list[field] = sample_list[field].to(
+                    device, non_blocking=True
+                )
 
         return sample_list
