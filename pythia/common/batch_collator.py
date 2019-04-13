18bddf2772e150c1b420da34e220faee03ad9508
Amanpreet Singh <apsdehal@gmail.com>
2019-04-12 17:59:25 -0700

    Update batch collator to add identical keys to SampleList only once

--- pythia/common/batch_collator.py
+++ pythia/common/batch_collator.py
@@ -2,5 +2,12 @@ from .sample import SampleList
 
 
 class BatchCollator:
+    # TODO: Think more if there is a better way to do this
+    _IDENTICAL_VALUE_KEYS = ["dataset_type", "dataset_name"]
     def __call__(self, batch):
-        return SampleList(batch)
+        sample_list = SampleList(batch)
+
+        for key in self._IDENTICAL_VALUE_KEYS:
+            sample_list[key] = sample_list[key][0]
+
+        return sample_list
