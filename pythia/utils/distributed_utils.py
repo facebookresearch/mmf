34307bf4bb05ab0e4385ccb905f8d968aca0d1e9
Amanpreet Singh <apsdehal@gmail.com>
2019-04-12 17:58:29 -0700

    Add reduce and gather distributed utilities

--- pythia/utils/distributed_utils.py
+++ pythia/utils/distributed_utils.py
@@ -1,8 +1,10 @@
 # Inspired from maskrcnn_benchmark
+import torch
+
 from torch import distributed as dist
 
 
-def synchronize(self):
+def synchronize():
     if not dist.is_nccl_available():
         return
     if not dist.is_initialized():
@@ -18,9 +20,9 @@ def synchronize(self):
 
 def get_rank():
     if not dist.is_nccl_available():
-        return
+        return 0
     if not dist.is_initialized():
-        return
+        return 0
     return dist.get_rank()
 
 
@@ -34,3 +36,34 @@ def get_world_size():
     if not dist.is_initialized():
         return 1
     return dist.get_world_size()
+
+
+def reduce_tensor(tensor):
+    world_size = get_world_size()
+
+    if world_size < 2:
+        return tensor
+
+    with torch.no_grad():
+        dist.reduce(tensor, dst=0)
+        if dist.get_rank() == 0:
+            tensor = tensor.div(world_size)
+
+    return tensor
+
+
+def gather_tensor(tensor):
+    world_size = get_world_size()
+
+    if world_size < 2:
+        return tensor
+
+    with torch.no_grad():
+        tensor_list = []
+
+        for _ in range(world_size):
+            tensor_list.append(torch.zeros_like(tensor))
+
+        dist.all_gather(tensor_list, tensor)
+        tensor_list = torch.stack(tensor_list, dim=0)
+    return tensor_list
