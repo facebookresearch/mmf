66516951a2ef2f2f4d59d6e5ed77e9e8b7a4e751
Amanpreet Singh <apsdehal@gmail.com>
2019-04-24 18:59:03 -0700

    Remove imports in main __init__

--- pythia/__init__.py
+++ pythia/__init__.py
@@ -1,9 +1,1 @@
-__all__ = ["utils", "modules", "models", "tasks", "core"]
-
 __version__ = "0.3.0"
-
-from . import utils
-from . import modules
-from . import models
-from . import tasks
-from . import common
