06c8a64841b5c8c4640253a2f8c8f1b9a1bee85f
Amanpreet Singh <apsdehal@gmail.com>
2019-04-24 19:22:05 -0700

    Use setuptools to find packages

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
-from setuptools import setup
+#!/usr/bin/env python3
 
-setup(name='pythia',
-      version='0.3',
-      author='Facebook AI Research',
-      license='BSD',
-      description="A modular research framework for multimodal vision and "
-      "language research.",
-      url="https://github.com/facebookresearch/pythia",
-      packages=["pythia"],
-      install_requires=['torch', 'torchtext', 'numpy',
-                        'torchvision', 'demjson', 'tensorboardX'])
+# Copyright (c) Facebook, Inc. and its affiliates.
+import os.path
+import sys
+
+import setuptools
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "pythia"))
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+with open("requirements.txt") as f:
+    reqs = f.read()
+
+DISTNAME = "pythia"
+DESCRIPTION = "pythia: a modular framework for vision and language multimodal \
+research."
+LONG_DESCRIPTION = readme
+AUTHOR = "Facebook AI Research"
+LICENSE = license
+REQUIREMENTS = (reqs.strip().split("\n"),)
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name=DISTNAME,
+        install_requires=REQUIREMENTS,
+        packages=setuptools.find_packages(),
+        version="0.3",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        license=LICENSE,
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest"],
+    )
