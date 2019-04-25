203bcd5fcac274f9e706273ea7676f2e3da37593
Amanpreet Singh <apsdehal@gmail.com>
2019-04-24 18:50:33 -0700

    Update setup.py to include more metadata and actual package register

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 
 setup(name='pythia',
       version='0.3',
+      author='Facebook AI Research',
+      license='BSD',
+      description="A modular research framework for multimodal vision and "
+      "language research.",
+      url="https://github.com/facebookresearch/pythia",
+      packages=["pythia"],
       install_requires=['torch', 'torchtext', 'numpy',
                         'torchvision', 'demjson', 'tensorboardX'])
