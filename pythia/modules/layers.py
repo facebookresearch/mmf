698101bf01844993be1666e18aa355c6d23dc098
Amanpreet Singh <apsdehal@gmail.com>
2019-06-18 22:57:40 -0700

    [feature] Add layers for ConvNet, Flatten and UnFlatten

--- pythia/modules/layers.py
+++ pythia/modules/layers.py
@@ -6,6 +6,44 @@ from pythia.common.registry import registry
 from pythia.modules.decoders import LanguageDecoder
 
 
+class ConvNet(nn.Module):
+    def __init__(
+        self,
+        in_c,
+        out_c,
+        kernel_size,
+        padding_size="same",
+        pool_stride=2,
+        batch_norm=True,
+    ):
+        super().__init__()
+
+        if padding_size == "same":
+            padding_size = kernel_size // 2
+        self.conv = nn.Conv2d(in_c, out_c, kernel_size, padding=padding_size)
+        self.max_pool2d = nn.MaxPool2d(pool_stride, stride=pool_stride)
+        self.batch_norm = batch_norm
+        self.batch_norm_2d = nn.BatchNorm2d(out_c)
+
+    def forward(self, x):
+        x = self.max_pool2d(nn.functional.leaky_relu(self.conv(x)))
+
+        if self.batch_norm:
+            return self.batch_norm_2d(x)
+        else:
+            return x
+
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class UnFlatten(nn.Module):
+    def forward(self, input, sizes=[]):
+        return input.view(input.size(0), *sizes)
+
+
 class GatedTanh(nn.Module):
     """
     From: https://arxiv.org/pdf/1707.07998.pdf
