b51f10a3d73cbc46c150eef52544b9ce91fd9b02
Amanpreet Singh <apsdehal@gmail.com>
2019-07-29 11:36:20 -0700

    [fix/feat] Add num_boxes and fix image width/height/conf thresh in feat extraction script
    - Fixes #120
    - Fixes #133
    
    - Also, returns number of boxes now in the feature info

--- pythia/scripts/features/extract_features_vmb.py
+++ pythia/scripts/features/extract_features_vmb.py
@@ -32,7 +32,6 @@ class FeatureExtractor:
     )
     MAX_SIZE = 1333
     MIN_SIZE = 800
-    NUM_FEATURES = 100
 
     def __init__(self):
         self.args = self.get_parser().parse_args()
@@ -58,6 +57,9 @@ class FeatureExtractor:
         )
         parser.add_argument("--batch_size", type=int, default=2, help="Batch size")
         parser.add_argument(
+            "--num_features", type=int, default=100, help="Number of features to extract."
+        )
+        parser.add_argument(
             "--output_folder", type=str, default="./output", help="Output folder"
         )
         parser.add_argument("--image_dir", type=str, help="Image directory or file")
@@ -66,7 +68,7 @@ class FeatureExtractor:
             default="fc6",
         )
         parser.add_argument(
-            "--confidence_threshold", type=float, default=0.2,
+            "--confidence_threshold", type=float, default=0,
             help="Threshold of detection confidence above which boxes will be selected"
         )
         parser.add_argument(
@@ -91,12 +93,14 @@ class FeatureExtractor:
     def _image_transform(self, path):
         img = Image.open(path)
         im = np.array(img).astype(np.float32)
-        # IndexError: too many indices for array
+        # IndexError: too many indices for array, grayscale images
         if len(im.shape) < 3:
             im = np.repeat(im[:, :, np.newaxis], 3, axis=2)
         im = im[:, :, ::-1]
         im -= np.array([102.9801, 115.9465, 122.7717])
         im_shape = im.shape
+        im_height = im_shape[0]
+        im_width = im_shape[1]
         im_size_min = np.min(im_shape[0:2])
         im_size_max = np.max(im_shape[0:2])
 
@@ -112,10 +116,16 @@ class FeatureExtractor:
             im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR
         )
         img = torch.from_numpy(im).permute(2, 0, 1)
-        return img, im_scale
+
+        im_info = {
+            "width": im_width,
+            "height": im_height
+        }
+
+        return img, im_scale, im_info
 
     def _process_feature_extraction(
-        self, output, im_scales, feature_name="fc6", conf_thresh=0.2
+        self, output, im_scales, im_infos, feature_name="fc6", conf_thresh=0
     ):
         batch_size = len(output[0]["proposals"])
         n_boxes_per_image = [len(boxes) for boxes in output[0]["proposals"]]
@@ -131,7 +141,7 @@ class FeatureExtractor:
             dets = output[0]["proposals"][i].bbox / im_scales[i]
             scores = score_list[i]
             max_conf = torch.zeros((scores.shape[0])).to(cur_device)
-
+            conf_thresh_tensor = torch.full_like(max_conf, conf_thresh)
             start_index = 1
             # Column 0 of the scores matrix is for the background class
             if self.args.background:
@@ -140,35 +150,40 @@ class FeatureExtractor:
                 cls_scores = scores[:, cls_ind]
                 keep = nms(dets, cls_scores, 0.5)
                 max_conf[keep] = torch.where(
-                    cls_scores[keep] > max_conf[keep], cls_scores[keep], max_conf[keep]
+                    # Better than max one till now and minimally greater than conf_thresh
+                    (cls_scores[keep] > max_conf[keep]) &
+                    (cls_scores[keep] > conf_thresh_tensor[keep]),
+                    cls_scores[keep], max_conf[keep]
                 )
 
-            keep_boxes = torch.argsort(max_conf, descending=True)[:self.NUM_FEATURES]
+            num_boxes = (torch.sort(max_conf, descending=True)[0][:self.args.num_features] != 0)
+            num_boxes = num_boxes.sum()
+            keep_boxes = torch.argsort(max_conf, descending=True)[:self.args.num_features]
             feat_list.append(feats[i][keep_boxes])
             bbox = output[0]["proposals"][i][keep_boxes].bbox / im_scales[i]
             # Predict the class label using the scores
             objects = torch.argmax(scores[keep_boxes][start_index:], dim=1)
-            image_width = output[0]["proposals"][i].size[0] / im_scales[i]
-            image_height = output[0]["proposals"][i].size[1] / im_scales[i]
 
             info_list.append(
                 {
                     "bbox": bbox.cpu().numpy(),
+                    "num_boxes": num_boxes.item(),
                     "objects": objects.cpu().numpy(),
-                    "image_width": image_width,
-                    "image_height": image_height,
+                    "image_width": im_infos[i]["width"],
+                    "image_height": im_infos[i]["height"],
                 }
             )
 
         return feat_list, info_list
 
     def get_detectron_features(self, image_paths):
-        img_tensor, im_scales = [], []
+        img_tensor, im_scales, im_infos = [], [], []
 
         for image_path in image_paths:
-            im, im_scale = self._image_transform(image_path)
+            im, im_scale, im_info = self._image_transform(image_path)
             img_tensor.append(im)
             im_scales.append(im_scale)
+            im_infos.append(im_info)
 
         # Image dimensions should be divisible by 32, to allow convolutions
         # in detector to work
@@ -177,9 +192,12 @@ class FeatureExtractor:
 
         with torch.no_grad():
             output = self.detection_model(current_img_list)
+
         feat_list = self._process_feature_extraction(
-            output, im_scales, self.args.feature_name, self.args.confidence_threshold
+            output, im_scales, im_infos, self.args.feature_name,
+            self.args.confidence_threshold
         )
+
         return feat_list
 
     def _chunks(self, array, chunk_size):
