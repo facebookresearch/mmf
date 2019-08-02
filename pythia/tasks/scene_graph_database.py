9b5572d814e361bf31dcb7530d42e0654002e1a5
Amanpreet Singh <apsdehal@gmail.com>
2019-08-02 14:16:41 -0700

    [feat] Load jsonl files in image database and scene graph database

---
+++ pythia/tasks/scene_graph_database.py
@@ -0,0 +1,15 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import copy
+
+from pythia.tasks.image_database import ImageDatabase
+
+
+class SceneGraphDatabase(ImageDatabase):
+    def __init__(self, scene_graph_path):
+        super().__init__(scene_graph_path)
+        self.data_dict = {}
+        for item in self.data:
+            self.data_dict[item["image_id"]] = item
+
+    def __getitem__(self, idx):
+        return copy.deepcopy(self.data_dict[idx])
