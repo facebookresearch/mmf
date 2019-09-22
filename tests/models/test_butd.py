98a36398b97dda435e6ca1a9652ec2ea9032992a
Ishan Maheshwari <ishan_bt2k15@dtu.ac.in>
2019-09-23 00:01:48 +0530

    test fix

--- tests/models/test_butd.py
+++ tests/models/test_butd.py
@@ -21,11 +21,15 @@ class TestModelBUTD(unittest.TestCase):
             get_pythia_root(), "..", "configs", "captioning", "coco", "butd_nucleus_sampling.yml"
         )
         config_path = os.path.abspath(config_path)
-        configuration = Configuration(config_path)
-        configuration.config["datasets"] = "coco"
+        with open(config_path) as f:
+            config = yaml.load(f)
 
-        self.config = configuration.config
-        registry.register("config", self.config)
+        config = ConfigNode(config)
+        # Remove warning
+        config.training_parameters.evalai_inference = True
+        registry.register("config", config)
+
+        self.config = config
 
         captioning_config = self.config.task_attributes.captioning.dataset_attributes.coco
         text_processor_config = captioning_config.processors.text_processor
