5f1883f090e021d9097712caf2b52d33ab46fdd6
Vedanuj Goswami <vedanujg@gmail.com>
2019-10-11 22:55:55 -0700

    [feature] Add CC dataset

--- pythia/datasets/captioning/coco/__init__.py
+++ pythia/datasets/captioning/coco/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-__all__ = ["COCOBuilder", "COCODataset"]
+__all__ = ["COCOBuilder", "COCODataset", "MaskedCOCOBuilder", "MaskedCOCODataset"]
 
 from .builder import COCOBuilder
 from .dataset import COCODataset
+from .masked_builder import MaskedCOCOBuilder
+from .masked_dataset import MaskedCOCODataset
---
+++ pythia/datasets/captioning/conceptual_captions/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+__all__ = ["MaskedCCBuilder", "MaskedCCDataset"]
+
+from .masked_builder import MaskedCCBuilder
+from .masked_dataset import MaskedCCDataset
