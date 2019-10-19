c6a8be7e8b3052c86de5424889fbbc2afc1d9ead
Vedanuj Goswami <vedanujg@gmail.com>
2019-10-18 21:53:47 -0700

    [feature] Add SBU Captions dataset and configs

---
+++ pythia/datasets/captioning/sbu_captions/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+__all__ = ["MaskedSBUBuilder", "MaskedSBUDataset"]
+
+from .masked_builder import MaskedSBUBuilder
+from .masked_dataset import MaskedSBUDataset
