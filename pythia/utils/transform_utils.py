d15210dbd96da58669050bb95781cdec70c3e1b7
Amanpreet Singh <apsdehal@gmail.com>
2020-03-17 15:27:10 -0700

    [feature] Add transform utilities package

---
+++ pythia/utils/transform_utils.py
@@ -0,0 +1,20 @@
+def transform_to_batch_sequence(tensor):
+    if tensor is not None:
+        if len(tensor.size()) == 2:
+            return tensor
+        else:
+            assert(len(tensor.size()) == 3)
+            return tensor.contiguous().view(-1, tensor.size(-1))
+    else:
+        return None
+
+
+def transform_to_batch_sequence_dim(tensor):
+    if tensor is not None:
+        if len(tensor.size()) == 3:
+            return tensor
+        else:
+            assert(len(tensor.size()) == 4)
+            return tensor.contiguous().view(-1, tensor.size(-2), tensor.size(-1))
+    else:
+        return None
