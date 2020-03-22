f81ed36d9d4f75cb4828f5d524cac1105ce72442
Ronghang Hu <ronghang.hu@gmail.com>
2020-03-22 15:10:12 -0700

    [refactor] rename m4c_stvqa, m4c_ocrvqa to stvqa and ocrvqa (#24)

---
+++ pythia/datasets/vqa/stvqa/builder.py
@@ -0,0 +1,12 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+from pythia.common.registry import Registry
+from pythia.datasets.vqa.stvqa.dataset import STVQADataset
+from pythia.datasets.vqa.m4c_textvqa.builder import M4CTextVQABuilder
+
+
+@Registry.register_builder("stvqa")
+class STVQABuilder(M4CTextVQABuilder):
+    def __init__(self):
+        super().__init__()
+        self.dataset_name = "stvqa"
+        self.set_dataset_class(STVQADataset)
