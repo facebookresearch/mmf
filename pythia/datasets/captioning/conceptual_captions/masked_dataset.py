073eaf3efcc8f7ce3f030792bff33188154d59a4
Vedanuj Goswami <vedanujg@gmail.com>
2020-04-03 18:02:23 -0700

    [feature] Add Conceptual Captions dataset (#57)
    * [feature] Add Conceptual Captions Dataset
    
    * Address comments

--- pythia/datasets/captioning/conceptual_captions/masked_dataset.py
+++ pythia/datasets/captioning/conceptual_captions/masked_dataset.py
@@ -3,10 +3,10 @@
 from pythia.datasets.captioning.coco import MaskedCOCODataset
 
 
-class MaskedCCDataset(MaskedCOCODataset):
+class MaskedConceptualCaptionsDataset(MaskedCOCODataset):
     def __init__(self, dataset_type, imdb_file_index, config, *args, **kwargs):
         super().__init__(dataset_type, imdb_file_index, config, *args, **kwargs)
-        self._name = "masked_cc"
+        self._name = "masked_conceptual_captions"
         self._two_sentence = config.get("two_sentence", True)
         self._false_caption = config.get("false_caption", True)
         self._two_sentence_probability = config.get("two_sentence_probability", 0.5)
