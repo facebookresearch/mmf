95ba241c3066f3b5bb8971cc2762e858cff26db5
Amanpreet Singh <apsdehal@gmail.com>
2020-04-09 12:21:35 -0700

    [feature] Add MMBT model (#64)
    This PR implements MMBT model described in https://arxiv.org/pdf/1909.02950.pdf.
    
    Salient features:
    - Two training modes: pretraining and classification
    - Works with both images as well as features
    - Can use any underlying encoder
    - Starter configurations provided for Hateful Memes and MaskedCOCO
    
    We provide two modes of training mmbt, pretraining and classification which are configurable through `training_head_type` configuration parameters. We also provide starter configuration that can be used to compose training configs for your use case. Sample configurations are provided for Hateful Memes and Masked COCO dataset. The model can work with either direct images or features provided from faster rcnn as is used in other Pythia models.
    
    A base is provided for easy building of further baselines. 
    
    Follow the steps in #62 to build the setup for hateful memes dataset and then test following commands:
    
    - For training on hateful memes with images:
    ```sh
    python -u tools/run.py training.batch_size=16 config=projects/mmbt/configs/hateful_memes/defaults.yaml  dataset=hateful_memes  model=mmbt training.log_interval=10 training.find_unused_parameters=True  training.num_workers=2
    ```
    
    - For training on hateful memes but with preextracted features:
    ```sh
    python -u tools/run.py training.batch_size=16 config=projects/mmbt/configs/hateful_memes/with_features.yaml  dataset=hateful_memes  model=mmbt training.log_interval=10 training.find_unused_parameters=True training.num_workers=2
    ```
    
    Both of the above commands invoke the classification MMBT model and #62 to be landed before they are run
    
    - For running pretraining on masked_coco with preextracted features run:
    ```sh
    python -u tools/run.py training.batch_size=16 config=projects/mmbt/configs/masked_coco/defaults.yaml  dataset=masked_coco  model=mmbt training.log_interval=10 training.find_unused_parameters=True training.num_workers=2
    ```

--- pythia/modules/encoders.py
+++ pythia/modules/encoders.py
@@ -3,10 +3,16 @@ import os
 import pickle
 
 import torch
+import torchvision
+from omegaconf import OmegaConf
 from torch import nn
+from transformers.configuration_auto import AutoConfig
+from transformers.file_utils import PYTORCH_PRETRAINED_BERT_CACHE
+from transformers.modeling_auto import AutoModel
 
 from pythia.modules.embeddings import ProjectionEmbedding
 from pythia.modules.layers import Identity
+from pythia.utils.build import build_image_encoder, build_text_encoder
 from pythia.utils.general import get_pythia_root
 
 
@@ -33,7 +39,9 @@ class ImageFeatureEncoder(nn.Module):
 
 
 class FinetuneFasterRcnnFpnFc7(nn.Module):
-    def __init__(self, in_dim, weights_file, bias_file, model_data_dir):
+    def __init__(
+        self, in_dim, weights_file, bias_file, model_data_dir, *args, **kwargs
+    ):
         super(FinetuneFasterRcnnFpnFc7, self).__init__()
         pythia_root = get_pythia_root()
         model_data_dir = os.path.join(pythia_root, model_data_dir)
@@ -57,3 +65,133 @@ class FinetuneFasterRcnnFpnFc7(nn.Module):
         i2 = self.lc(image)
         i3 = nn.functional.relu(i2)
         return i3
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__()
+        self._type = config.type
+        params = config.params
+
+        if self._type == "default":
+            self.module = nn.Identity()
+            self.module.out_dim = params.in_dim
+        elif self._type == "resnet152":
+            self.module = ResNet152ImageEncoder(params)
+        else:
+            raise NotImplementedError("Unknown Image Encoder: %s" % self._type)
+
+    @property
+    def out_dim(self):
+        return self.module.out_dim
+
+    def forward(self, image):
+        return self.module(image)
+
+
+# Taken from facebookresearch/mmbt with some modifications
+class ResNet152ImageEncoder(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__()
+        self.config = config
+        model = torchvision.models.resnet152(pretrained=config.get("pretrained", True))
+        modules = list(model.children())[:-2]
+        self.model = nn.Sequential(*modules)
+
+        pool_func = (
+            nn.AdaptiveAvgPool2d if config.pool_type == "avg" else nn.AdaptiveMaxPool2d
+        )
+
+        # -1 will keep the original feature size
+        if config.num_output_features == -1:
+            self.pool = nn.Identity()
+        elif config.num_output_features in [1, 2, 3, 5, 7]:
+            self.pool = pool_func((config.num_output_features, 1))
+        elif config.num_output_features == 4:
+            self.pool = pool_func((2, 2))
+        elif config.num_output_features == 6:
+            self.pool = pool_func((3, 2))
+        elif config.num_output_features == 8:
+            self.pool = pool_func((4, 2))
+        elif config.num_output_features == 9:
+            self.pool = pool_func((3, 3))
+
+        self.out_dim = 2048
+
+    def forward(self, x):
+        # Bx3x224x224 -> Bx2048x7x7 -> Bx2048xN -> BxNx2048
+        out = self.pool(self.model(x))
+        out = torch.flatten(out, start_dim=2)
+        out = out.transpose(1, 2).contiguous()
+        return out  # BxNx2048
+
+
+class TextEncoder(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__()
+        self._type = config.type
+
+        if self._type == "identity":
+            self.module = nn.Identity()
+        elif self._type == "transformer":
+            self._module = TransformerEncoder(config.params)
+            self.module = self._module.module
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__()
+        self.config = config
+        self.module = AutoModel.from_pretrained(
+            self.config.bert_model_name,
+            config=self._build_encoder_config(config),
+            cache_dir=os.path.join(
+                str(PYTORCH_PRETRAINED_BERT_CACHE), "distributed_{}".format(-1)
+            ),
+        )
+        self.embeddings = self.module.embeddings
+        self.config = self.module.config
+
+    def _build_encoder_config(self, config):
+        return AutoConfig.from_pretrained(
+            self.config.bert_model_name, **OmegaConf.to_container(self.config)
+        )
+
+    def forward(self, *args, **kwargs):
+        # Only return pooled output
+        return self.module(*args, **kwargs)[1]
+
+
+class MultiModalEncoderBase(nn.Module):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__()
+        self.config = config
+        self._modal_encoder_config = self.config.modal_encoder
+        self._is_direct_features_input = self.config.direct_features_input
+
+        self.build()
+        self.modal_hidden_size = self.config.get("modal_hidden_size", None)
+        self.text_hidden_size = self.config.get("text_hidden_size", None)
+
+    def build(self):
+        encoders = self._build_encoders(self.config)
+        self.text_encoder, self.modal_encoder = encoders[0], encoders[1]
+        self._encoder_config = self.text_encoder.config
+
+    @property
+    def encoder_config(self):
+        return self._encoder_config
+
+    def _build_encoders(self, config):
+        return (
+            build_text_encoder(config.text_encoder),
+            self._build_modal_encoder(config.modal_encoder),
+        )
+
+    def _build_modal_encoder(self, config):
+        return build_image_encoder(
+            config, direct_features=self._is_direct_features_input
+        )
