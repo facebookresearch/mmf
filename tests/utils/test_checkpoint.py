a2160d79abede494884892a3fbbd3312d48ab53d
Amanpreet Singh <apsdehal@gmail.com>
2020-06-04 23:21:54 -0700

    Update

--- tests/utils/test_checkpoint.py
+++ tests/utils/test_checkpoint.py
@@ -195,19 +195,17 @@ class TestUtilsCheckpoint(unittest.TestCase):
             # Test distributed settings
             self.trainer.model = torch.nn.DataParallel(self.trainer.model)
             checkpoint.load_state_dict()
-            
+
             weight_to_be_tested = self.trainer.model.module.base[0].weight
             weight_device = weight_to_be_tested.device
-            
+
             self.assertTrue(
                 torch.equal(
                     weight_to_be_tested, base_0_weight_current.to(weight_device)
                 )
             )
             self.assertFalse(
-                torch.equal(
-                    weight_to_be_tested, base_0_weight_best.to(weight_device)
-                )
+                torch.equal(weight_to_be_tested, base_0_weight_best.to(weight_device))
             )
 
     def test_finalize_and_restore_from_it(self):
