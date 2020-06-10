c63742723b1bca5755d272bfe09f830d27fdc1a3
Amanpreet Singh <apsdehal@gmail.com>
2020-06-09 22:20:44 -0700

    Deploy website - based on 6c014dca4108fe4f0a368a27cdcf6d88fe3bf7c2

--- api/_static/js/ga.js
+++ api/_static/js/ga.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'UA-135079836-2');
+gtag('config', 'UA-135079836-3');
