0bff65a421a795187172695867db1d787ea914c9
Karan Desai <kddeskar@gmail.com>
2020-06-16 21:19:21 -0400

    Add TorchvisionImageEncoder, PyTorchTransformerDecoder and embedding.

--- mmf/modules/decoders.py
+++ mmf/modules/decoders.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.nn.utils.weight_norm import weight_norm
 
 from mmf.common.registry import registry
+from mmf.modules.embeddings import TextEmbedding
 
 
 class VisDialDiscriminator(nn.Module):
@@ -81,3 +82,124 @@ class LanguageDecoder(nn.Module):
         state["lm_hidden"] = (h2, c2)
 
         return predictions
+
+
+class PyTorchTransformerDecoder(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        hidden_size: int,
+        num_layers: int,
+        attention_heads: int,
+        feedforward_size: int,
+        dropout: float = 0.1,
+        padding_idx: int = 0,
+        max_sequence_length: int = 30,
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.attention_heads = attention_heads
+        self.feedforward_size = feedforward_size
+        self.dropout = dropout
+        self.padding_idx = padding_idx
+
+        # Create a randomly initialized word + positional embedding.
+        self.embedding = TextEmbedding(
+            "wordandpos",
+            vocab_size=self.vocab_size,
+            embedding_dim=self.hidden_size,
+            max_sequence_length=max_sequence_length,
+            dropout=dropout,
+            padding_idx=padding_idx,
+        )
+        self.transformer = nn.TransformerDecoder(
+            nn.TransformerDecoderLayer(
+                self.hidden_size,
+                self.attention_heads,
+                dim_feedforward=self.feedforward_size,
+                dropout=dropout,
+                activation="gelu",
+            ),
+            self.num_layers
+        )
+        self.apply(self.init_weights)
+
+        # Create an output linear layer and tie the input and output word
+        # embeddings to reduce parameters.
+        self.output = nn.Linear(self.textual_feature_size, vocab_size)
+        self.output.weight = self.embedding.words.weight
+
+    @staticmethod
+    def init_weights(module):
+        r"""Initialize weights like BERT - N(0.0, 0.02), bias = 0."""
+
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+        elif isinstance(module, nn.MultiheadAttention):
+            module.in_proj_weight.data.normal_(mean=0.0, std=0.02)
+            module.out_proj.weight.data.normal_(mean=0.0, std=0.02)
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+
+    def forward(
+        self,
+        encoded_features: torch.Tensor,
+        sequence_tokens: torch.Tensor,
+        sequence_lengths: torch.Tensor,
+    ) -> torch.Tensor:
+        batch_size, max_sequence_length = sequence_tokens.size()
+
+        # Create a mask based on sequence lengths, shape: (batch_size, )
+        # Form a binary mask: it is True for padding positions.
+        # These positions will be ignored for multi-headed attention.
+        ones = torch.ones_like(sequence_tokens)
+        sequence_mask = sequence_lengths.unsqueeze(1) < ones.cumsum(dim=1)
+
+        # shape: (batch_size, max_sequence_length, textual_feature_size)
+        embeddings = self.embedding(sequence_tokens)
+
+        # An additive mask for masking the future (one direction).
+        unidirectional_mask = self._generate_future_mask(
+            max_sequence_length, embeddings.dtype, embeddings.device
+        )
+        # We transpose the first two dimensions of tokens embeddings and encoded
+        # features, as required by transformer.
+        embeddings = embeddings.transpose(0, 1)
+        encoded_features = encoded_features.transpose(0, 1)
+
+        # shape: (max_sequence_length, batch_size, hidden_size)
+        textual_features = self.transformer(
+            embeddings,
+            encoded_features,
+            tgt_mask=unidirectional_mask,
+            tgt_key_padding_mask=sequence_mask,
+        )
+        # Undo the transpose and bring batch to dim 0.
+        # shape: (batch_size, max_sequence_length, hidden_size)
+        textual_features = textual_features.transpose(0, 1)
+
+        # shape: (batch_size, max_sequence_length, vocab_size)
+        output_logits = self.output(textual_features)
+        return output_logits
+
+    def _generate_future_mask(
+        self, size: int, dtype: torch.dtype, device: torch.device
+    ) -> torch.Tensor:
+        r"""
+        Generate a mask for "future" positions, useful when using this module
+        for language modeling.
+
+        Parameters
+        ----------
+        size: int
+        """
+        # Default mask is for forward direction. Flip for backward direction.
+        mask = torch.triu(
+            torch.ones(size, size, device=device, dtype=dtype), diagonal=1
+        )
+        mask = mask.masked_fill(mask == 1, float("-inf"))
+        return mask
--- mmf/modules/embeddings.py
+++ mmf/modules/embeddings.py
@@ -43,6 +43,9 @@ class TextEmbedding(nn.Module):
             embedding_dim = kwargs["embedding_dim"]
             self.module = nn.Embedding(vocab_size, embedding_dim)
             self.module.text_out_dim = self.embedding_dim
+        elif emb_type == "wordandpos":
+            self.module = WordAndPositionalEmbedding(**kwargs)
+            self.module.text_out_dim = self.embedding_dim
         else:
             raise NotImplementedError("Unknown question embedding '%s'" % emb_type)
 
@@ -424,3 +427,95 @@ class BertVisioLinguisticEmbeddings(BertEmbeddings):
         embeddings = self.LayerNorm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
+
+
+class WordAndPositionalEmbedding(nn.Module):
+    r"""
+    A :class:`~torch.nn.Module` for learned word embeddings and position
+    embeddings for input tokens. Each token is mapped to a fixed dimensional
+    word embedding; and corresponding positional embedding based on its index.
+    These are summed together followed by layer normalization and an optional
+    dropout.
+
+    Parameters
+    ----------
+    vocab_size: int
+        Size of token vocabulary.
+    embedding_dim: int
+        Dimension of token embedding vectors.
+    max_sequence_length: int, optional (default = 30)
+        Maximum length of input sequence; this is used to create a fixed
+        positional embedding lookup table.
+    dropout: float, optional (default = 0.1)
+        Dropout probability for final dropout applied after layer normalization.
+    padding_idx: int, optional (default = 0)
+        Token index of ``[PAD]`` token, word embedding for these tokens will
+        be a vector of zeroes (and not trainable).
+    """
+    def __init__(
+        self,
+        vocab_size: int,
+        embedding_dim: int,
+        max_sequence_length: int = 30,
+        dropout: float = 0.0,
+        padding_idx: int = 0,
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.padding_idx = padding_idx
+
+        self.words = nn.Embedding(vocab_size, hidden_size, padding_idx=padding_idx)
+
+        # We provide no "padding index" for positional embeddings. We zero out
+        # the positional embeddings of padded positions as a post-processing.
+        self.positions = nn.Embedding(max_sequence_length, hidden_size)
+        self.layer_norm = nn.LayerNorm(
+            hidden_size, eps=1e-8, elementwise_affine=True
+        )
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, tokens: torch.Tensor) -> torch.Tensor:
+        r"""
+        Get combined word and positional embeddings for input tokens.
+
+        Parameters
+        ----------
+        tokens: torch.Tensor
+            A tensor of shape ``(batch_size, max_sequence_length)`` containing
+            a batch of sequence tokens, with values in ``[0, vocab_size)``.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor of shape ``(batch_size, max_sequence_length, hidden_size)``
+            containing corresponding token embeddings.
+        """
+        position_indices = self._create_position_indices(tokens)
+
+        # shape: (batch_size, max_caption_length, hidden_size)
+        word_embeddings = self.words(tokens)
+        position_embeddings = self.positions(position_indices)
+
+        # shape: (batch_size, max_caption_length, hidden_size)
+        embeddings = self.layer_norm(word_embeddings + position_embeddings)
+        embeddings = self.dropout(embeddings)
+
+        # Zero-out embeddings for positions which have padding tokens.
+        # shape: (batch_size, max_caption_length, 1)
+        token_mask = (tokens != self.padding_idx).unsqueeze(-1)
+
+        # shape: (batch_size, max_caption_length, hidden_size)
+        embeddings = embeddings * token_mask.type(embeddings.dtype)
+        return embeddings
+
+    @functools.lru_cache(maxsize=128)
+    def _create_position_indices(self, tokens: torch.Tensor):
+
+        # Create position indices of the same size as token indices.
+        batch_size, max_caption_length = tokens.size()
+        positions = torch.arange(
+            max_caption_length, dtype=tokens.dtype, device=tokens.device
+        )
+        # shape: (batch_size, max_caption_length)
+        positions = positions.unsqueeze(0).expand(batch_size, max_caption_length)
+        return positions
