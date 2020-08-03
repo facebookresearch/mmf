efa331e808ad99bdb9eea3ba1dec00264fe1be00
Charles Bai <charlesxbai@fb.com>
2020-08-03 11:17:30 -0700

    [fix] Override of final array elements through dot element (#438)
    Summary:
    Should check if we reach the end of the dot_list when overriding config with args. This should fix issue https://github.com/facebookresearch/mmf/issues/431
    
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/438
    
    Test Plan:
    ```
    mmf_run config=projects/m4c/configs/textvqa/defaults.yaml \
        datasets=textvqa \
        model=m4c \
        run_type=train_val \
        training.lr_steps[1]=100 \
        dataset_config.textvqa.zoo_requirements[0]="test" \
    ```
    
    Output:
    ```
    Overriding option config to projects/m4c/configs/textvqa/defaults.yaml
    Overriding option datasets to textvqa
    Overriding option model to m4c
    Overriding option run_type to train_val
    Overriding option training.lr_steps[1] to 100
    Overriding option dataset_config.textvqa.zoo_requirements[0] to test
    ```
    
    Also: `pytest ./tests/configs/test_config_overrides.py`
    
    Reviewed By: apsdehal
    
    Differential Revision: D22894342
    
    Pulled By: charlesxbai
    
    fbshipit-source-id: bbd67b0c223c571bd2f705299d4a48cf48a49655

--- mmf/utils/configuration.py
+++ mmf/utils/configuration.py
@@ -420,7 +420,7 @@ class Configuration:
                     if not isinstance(
                         current_value,
                         (collections.abc.Mapping, collections.abc.Sequence),
-                    ):
+                    ) or idx == len(splits) - 1:
                         logger.info(f"Overriding option {opt} to {value}")
                         current[stripped_field][array_index] = self._decode_value(value)
                     else:
