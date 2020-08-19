962a7ea906156f4055afff73b0b444e17d8b758a
Suzanna Sia <suzannasia@fb.com>
2020-08-18 17:43:08 -0700

    [fix] More informative logging for loading checkpoints. (#491)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/491
    
    Pull Request resolved: https://github.com/fairinternal/mmf-internal/pull/131
    
    Previously unclear at what stage the loaded checkpoint file resumes training and this may have some unintended consequences.
    
    Adding `num_updates`, `current_iteration`, and `current_epoch` to the logger.
    
    Reviewed By: vedanuj
    
    Differential Revision: D23199211
    
    fbshipit-source-id: c224d169f2aad681e235a3b820b2b824b5021055

--- mmf/utils/checkpoint.py
+++ mmf/utils/checkpoint.py
@@ -208,15 +208,17 @@ class Checkpoint:
                 self._load_optimizer(ckpt)
 
             self.trainer.early_stop_callback.early_stopping.init_from_checkpoint(ckpt)
-
-            logger.info("Checkpoint loaded")
-
             reset_counts = ckpt_config.reset.all or ckpt_config.reset.counts
 
             if not reset_counts:
                 self._load_counts(ckpt)
         else:
             self._load_pretrained(new_dict)
+
+        logger.info("Checkpoint loaded.")
+        logger.info(f"Current num updates: {self.trainer.num_updates}")
+        logger.info(f"Current iteration: {self.trainer.current_iteration}")
+        logger.info(f"Current epoch: {self.trainer.current_epoch}")
 
     def _load_optimizer(self, ckpt):
         if "optimizer" in ckpt:
