a67ca624be60f15cee75842fb43de001729244ae
Ronghang Hu <ronghang.hu@gmail.com>
2020-09-10 09:32:50 -0700

    [fixup] fix config and zoo file for m4c_captioner (#553)
    Summary:
    - Fix the the mismatch between M4C-Captioner checkpoint and TextVQA features (https://github.com/facebookresearch/mmf/issues/544). Keep two variants of M4C-Captioner config, one trained with newer features extracted with maskrcnn-benchmark (`defaults`), and the other trained with older features extracted with Caffe2 (`with_caffe2_feat`), which is used in our experimentations in the paper and has higher CIDEr.
    - Create a MMF website page for M4C-Captioner.
    - Also fix previous M4C website page.
    
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/553
    
    Test Plan: - tested locally with model zoo checkpoints `m4c_captioner.textcaps.defaults` (for `defaults.yaml`) and `m4c_captioner.textcaps.with_caffe2_feat` (for `with_caffe2_feat.yaml`)
    
    Reviewed By: apsdehal
    
    Differential Revision: D23620758
    
    Pulled By: ronghanghu
    
    fbshipit-source-id: a5c7df33d8424da1e3d0bcb1030a3e621487cb27

--- website/sidebars.js
+++ website/sidebars.js
@@ -46,6 +46,6 @@ module.exports = {
       'challenges/textvqa_challenge',
       'challenges/vqa_challenge',
     ],
-    Projects: ['projects/butd', 'projects/m4c', 'projects/movie_mcan'],
+    Projects: ['projects/butd', 'projects/m4c', 'projects/m4c_captioner', 'projects/movie_mcan'],
   },
 };
