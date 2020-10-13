4076455f7519780451276d77d4a32023ae353281
Amanpreet Singh <apsdehal@gmail.com>
2020-10-12 17:00:21 -0700

    [feat] Enable torchscript on full VisualBERT model
    - This removes opinions on module getting torchscripted rather than the
    full model. This will also enable us to keep the signature of Dict[str,
    Tensor] same for all of the MMF models
    - Modifies code flow according to scripting model and pretraining model
    conditions
    - Add helps function around getattr
    - Modifies the test to play nice with new API which looks better
    
    Test Plan:
    Modifies VisualBERT tests for TorchScript to script and test the whole
    model

--- tests/models/test_visual_bert.py
+++ tests/models/test_visual_bert.py
@@ -6,6 +6,8 @@ import unittest
 import tests.test_utils as test_utils
 import torch
 from mmf.common.registry import registry
+from mmf.common.sample import SampleList
+from mmf.modules.hf_layers import replace_with_jit
 from mmf.utils.configuration import Configuration
 from mmf.utils.env import setup_imports
 
@@ -13,6 +15,7 @@ from mmf.utils.env import setup_imports
 class TestVisualBertTorchscript(unittest.TestCase):
     def setUp(self):
         setup_imports()
+        replace_with_jit()
         model_name = "visual_bert"
         args = test_utils.dummy_args(model=model_name)
         configuration = Configuration(args)
@@ -27,58 +30,41 @@ class TestVisualBertTorchscript(unittest.TestCase):
         self.finetune_model.build()
 
     @test_utils.skip_if_no_network
-    def test_load_save_pretrain_model(self):
-        self.pretrain_model.model.eval()
-        script_model = torch.jit.script(self.pretrain_model.model)
-        buffer = io.BytesIO()
-        torch.jit.save(script_model, buffer)
-        buffer.seek(0)
-        loaded_model = torch.jit.load(buffer)
-        self.assertTrue(test_utils.assertModulesEqual(script_model, loaded_model))
+    def test_pretrained_model_jit_assertion(self):
+        self.pretrain_model.eval()
+        self.assertRaises(RuntimeError, torch.jit.script(self.pretrain_model))
 
     @test_utils.skip_if_no_network
     def test_pretrained_model(self):
-        self.pretrain_model.model.eval()
+        self.pretrain_model.eval()
 
-        input_ids = torch.randint(low=0, high=30255, size=(1, 128)).long()
-        input_mask = torch.ones((1, 128)).long()
-        attention_mask = torch.ones((1, 228)).long()
-        token_type_ids = torch.zeros(1, 128).long()
-        visual_embeddings = torch.rand((1, 100, 2048)).float()
-        visual_embeddings_type = torch.zeros(1, 100).long()
-        masked_lm_labels = torch.zeros((1, 228), dtype=torch.long).fill_(-1)
+        sample_list = SampleList()
+
+        sample_list.add_field(
+            "input_ids", torch.randint(low=0, high=30255, size=(1, 128)).long()
+        )
+        sample_list.add_field("input_mask", torch.ones((1, 128)).long())
+        sample_list.add_field("segment_ids", torch.zeros(1, 128).long())
+        sample_list.add_field("image_feature_0", torch.rand((1, 100, 2048)).float())
+        sample_list.add_field(
+            "lm_label_ids", torch.zeros((1, 128), dtype=torch.long).fill_(-1)
+        )
 
         self.pretrain_model.eval()
 
+        sample_list.dataset_name = "random"
+        sample_list.dataset_type = "test"
         with torch.no_grad():
-            model_output = self.pretrain_model.model(
-                input_ids=input_ids,
-                input_mask=input_mask,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                visual_embeddings=visual_embeddings,
-                visual_embeddings_type=visual_embeddings_type,
-                masked_lm_labels=masked_lm_labels,
-            )
-        script_model = torch.jit.script(self.pretrain_model.model)
-        with torch.no_grad():
-            script_output = script_model(
-                input_ids=input_ids,
-                input_mask=input_mask,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                visual_embeddings=visual_embeddings,
-                visual_embeddings_type=visual_embeddings_type,
-                masked_lm_labels=masked_lm_labels,
-            )
-        self.assertEqual(
-            model_output["masked_lm_loss"], script_output["masked_lm_loss"]
-        )
+            model_output = self.pretrain_model(sample_list)
+
+        self.assertTrue("losses" in model_output)
+        self.assertTrue("random/test/masked_lm_loss" in model_output["losses"])
+        self.assertTrue(model_output["losses"]["random/test/masked_lm_loss"] == 0)
 
     @test_utils.skip_if_no_network
     def test_load_save_finetune_model(self):
-        self.finetune_model.model.eval()
-        script_model = torch.jit.script(self.finetune_model.model)
+        self.finetune_model.eval()
+        script_model = torch.jit.script(self.finetune_model)
         buffer = io.BytesIO()
         torch.jit.save(script_model, buffer)
         buffer.seek(0)
@@ -87,34 +73,21 @@ class TestVisualBertTorchscript(unittest.TestCase):
 
     @test_utils.skip_if_no_network
     def test_finetune_model(self):
+        self.finetune_model.eval()
+        sample_list = SampleList()
 
-        self.finetune_model.model.eval()
-        input_ids = torch.randint(low=0, high=30255, size=(1, 128)).long()
-        input_mask = torch.ones((1, 128)).long()
-        attention_mask = torch.ones((1, 228)).long()
-        token_type_ids = torch.zeros(1, 128).long()
-        visual_embeddings = torch.rand((1, 100, 2048)).float()
-        visual_embeddings_type = torch.zeros(1, 100).long()
+        sample_list.add_field(
+            "input_ids", torch.randint(low=0, high=30255, size=(1, 128)).long()
+        )
+        sample_list.add_field("input_mask", torch.ones((1, 128)).long())
+        sample_list.add_field("segment_ids", torch.zeros(1, 128).long())
+        sample_list.add_field("image_feature_0", torch.rand((1, 100, 2048)).float())
 
         with torch.no_grad():
-            model_output = self.finetune_model.model(
-                input_ids=input_ids,
-                input_mask=input_mask,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                visual_embeddings=visual_embeddings,
-                visual_embeddings_type=visual_embeddings_type,
-            )
+            model_output = self.finetune_model(sample_list)
 
-        script_model = torch.jit.script(self.finetune_model.model)
+        script_model = torch.jit.script(self.finetune_model)
         with torch.no_grad():
-            script_output = script_model(
-                input_ids=input_ids,
-                input_mask=input_mask,
-                attention_mask=attention_mask,
-                token_type_ids=token_type_ids,
-                visual_embeddings=visual_embeddings,
-                visual_embeddings_type=visual_embeddings_type,
-            )
+            script_output = script_model(sample_list)
 
         self.assertTrue(torch.equal(model_output["scores"], script_output["scores"]))
