ec5c8b56763d6e494db2431bf471c05a647095ff
Sasha Sheng <sash@fb.com>
2020-12-07 18:20:13 -0800

    [feat] allow passing in model's file path for from_pretrained (#677)
    Summary:
    - from_pretrained to allow a filepath
    - add test
    
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/677
    
    Reviewed By: apsdehal
    
    Differential Revision: D25167229
    
    Pulled By: ytsheng
    
    fbshipit-source-id: 2bc976e6239b4d6bd23b7e0b9f3d8495e9381d2d

--- tests/models/interfaces/test_interfaces.py
+++ tests/models/interfaces/test_interfaces.py
@@ -1,10 +1,16 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
+import os
+import tempfile
 import unittest
+from pathlib import Path
 
 import numpy as np
 import tests.test_utils as test_utils
+import torch
 from mmf.models.mmbt import MMBT
+from mmf.utils.configuration import get_mmf_env, load_yaml
+from mmf.utils.file_io import PathManager
 
 
 class TestModelInterfaces(unittest.TestCase):
@@ -13,10 +19,22 @@ class TestModelInterfaces(unittest.TestCase):
     @test_utils.skip_if_macos
     def test_mmbt_hm_interface(self):
         model = MMBT.from_pretrained("mmbt.hateful_memes.images")
+        self._test_model_performance(model)
+
+    @test_utils.skip_if_no_network
+    @test_utils.skip_if_windows
+    @test_utils.skip_if_macos
+    def test_mmbt_hm_interface_from_file(self):
+        with tempfile.NamedTemporaryFile(suffix=".pth") as tmp:
+            self._create_checkpoint_file(tmp.name)
+
+            model = MMBT.from_pretrained(tmp.name, interface=True)
+            self._test_model_performance(model)
+
+    def _test_model_performance(self, model):
         result = model.classify(
             "https://i.imgur.com/tEcsk5q.jpg", "look how many people love you"
         )
-
         self.assertEqual(result["label"], 0)
         np.testing.assert_almost_equal(result["confidence"], 0.9993, decimal=3)
         result = model.classify(
@@ -27,3 +45,17 @@ class TestModelInterfaces(unittest.TestCase):
         result = model.classify("https://i.imgur.com/tEcsk5q.jpg", "hitler and jews")
         self.assertEqual(result["label"], 1)
         np.testing.assert_almost_equal(result["confidence"], 0.8342, decimal=3)
+
+    def _create_checkpoint_file(self, path):
+        home = str(Path.home())
+        data_dir = get_mmf_env(key="data_dir")
+        model_folder = os.path.join(
+            home, data_dir, "models", "mmbt.hateful_memes.images"
+        )
+        model_file = os.path.join(model_folder, "model.pth")
+        config_file = os.path.join(model_folder, "config.yaml")
+        config = load_yaml(config_file)
+        with PathManager.open(model_file, "rb") as f:
+            ckpt = torch.load(f)
+        ckpt["config"] = config
+        torch.save(ckpt, path)
