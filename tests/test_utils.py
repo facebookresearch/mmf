[fix] gradient accumulation when update_frequency != 1

tests/test_utils.py
@@ -117,12 +117,13 @@ class NumbersDataset(torch.utils.data.Dataset):
 class SimpleModel(torch.nn.Module):
     def __init__(self, size):
         super().__init__()
-        self.linear = torch.nn.Linear(size, 4)
+        self.linear = torch.nn.Linear(size, 1)
 
     def forward(self, prepared_batch):
         batch = prepared_batch[DATA_ITEM_KEY]
-        model_output = {"losses": {"loss": torch.sum(self.linear(batch))}}
-        return model_output
+        output = self.linear(batch)
+        loss = torch.nn.MSELoss()(output, batch)
+        return {"losses": {"loss": loss}, "logits": output}
 
 
 def assertModulesEqual(mod1, mod2):

tests/trainers/test_training_loop.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.mock import MagicMock
 
 import torch
+from mmf.common.meter import Meter
 from mmf.common.sample import SampleList
 from mmf.trainers.core.profiling import TrainerProfilingMixin
 from mmf.trainers.core.training_loop import TrainerTrainingLoopMixin
@@ -12,15 +13,31 @@ from tests.test_utils import NumbersDataset, SimpleModel
 
 
 class TrainerTrainingLoopMock(TrainerTrainingLoopMixin, TrainerProfilingMixin):
-    def __init__(self, num_train_data, max_updates, max_epochs):
-        self.training_config = OmegaConf.create(
-            {
-                "detect_anomaly": False,
-                "evaluation_interval": 10000,
-                "update_frequency": 1,
-                "fp16": True,
-            }
-        )
+    def __init__(
+        self,
+        num_train_data,
+        max_updates,
+        max_epochs,
+        config=None,
+        optimizer=None,
+        update_frequency=1,
+        batch_size=1,
+        fp16=False,
+        on_update_end_fn=None,
+    ):
+        if config is None:
+            self.training_config = OmegaConf.create(
+                {
+                    "detect_anomaly": False,
+                    "evaluation_interval": 10000,
+                    "update_frequency": update_frequency,
+                    "fp16": fp16,
+                    "batch_size": batch_size,
+                }
+            )
+        else:
+            self.training_config = config.training
+
         if max_updates is not None:
             self.training_config["max_updates"] = max_updates
         if max_epochs is not None:
@@ -29,16 +46,27 @@ class TrainerTrainingLoopMock(TrainerTrainingLoopMixin, TrainerProfilingMixin):
         self.model = SimpleModel(1)
         if torch.cuda.is_available():
             self.model = self.model.cuda()
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
 
         self.dataset_loader = MagicMock()
         self.dataset_loader.seed_sampler = MagicMock(return_value=None)
         self.dataset_loader.prepare_batch = lambda x: SampleList(x)
-        self.optimizer = MagicMock()
-        self.optimizer.step = MagicMock(return_value=None)
-        self.optimizer.zero_grad = MagicMock(return_value=None)
+        if optimizer is None:
+            self.optimizer = MagicMock()
+            self.optimizer.step = MagicMock(return_value=None)
+            self.optimizer.zero_grad = MagicMock(return_value=None)
+        else:
+            self.optimizer = optimizer
+
         dataset = NumbersDataset(num_train_data)
         self.train_loader = torch.utils.data.DataLoader(
-            dataset=dataset, batch_size=1, shuffle=False, num_workers=1, drop_last=False
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=1,
+            drop_last=False,
         )
         self.train_loader.current_dataset = dataset
         self.on_batch_start = MagicMock(return_value=None)
@@ -46,17 +74,113 @@ class TrainerTrainingLoopMock(TrainerTrainingLoopMixin, TrainerProfilingMixin):
         self.logistics_callback = MagicMock(return_value=None)
         self.logistics_callback.log_interval = MagicMock(return_value=None)
         self.on_batch_end = MagicMock(return_value=None)
-        self.on_update_end = MagicMock(return_value=None)
-        self.meter = MagicMock(return_value=None)
-        loss_mock = MagicMock()
-        loss_mock.backward = MagicMock()
+        self.on_update_end = (
+            on_update_end_fn if on_update_end_fn else MagicMock(return_value=None)
+        )
+        self.meter = Meter()
         self.after_training_loop = MagicMock(return_value=None)
-        self.scaler = MagicMock()
-        self.scaler.scale = MagicMock(return_value=loss_mock)
-        self.scaler.step = MagicMock(return_value=None)
+        self.on_validation_start = MagicMock(return_value=None)
+        self.evaluation_loop = MagicMock(return_value=(None, None))
+        self.scaler = torch.cuda.amp.GradScaler(enabled=False)
+        self.val_loader = MagicMock(return_value=None)
+        self.early_stop_callback = MagicMock(return_value=None)
+        self.on_validation_end = MagicMock(return_value=None)
+        self.metrics = MagicMock(return_value=None)
 
 
 class TestTrainingLoop(unittest.TestCase):
+    def test_update_frequency_num_remaining_updates_greater_than_update_frequency(self):
+        trainer1 = self._train_with_condition(
+            num_train_data=20,
+            max_updates=None,
+            max_epochs=2,
+            update_frequency=3,
+            batch_size=6,
+        )
+        self.assertEqual(trainer1.num_updates, 4)
+
+        trainer2 = self._train_with_condition(
+            num_train_data=20,
+            max_updates=4,
+            max_epochs=None,
+            update_frequency=1,
+            batch_size=18,
+        )
+        self.assertEqual(trainer2.num_updates, 4)
+        self._compare_model_params(trainer1, trainer2)
+
+    def test_update_frequency_reporting(self):
+        def _on_update_end(report, meter, should_log):
+            # the losses here should be the sum of two losses in
+            # iteration 0 and iteration 1 (both constitute update 0).
+            # Here iter 1 loss: 0.2684, iter 2 loss: 2.4167
+            loss = report.losses["loss"].detach().cpu().item()
+            self.assertAlmostEqual(loss, 2.6852, 4)
+
+        self._train_with_condition(
+            num_train_data=100,
+            max_updates=1,
+            max_epochs=None,
+            update_frequency=2,
+            batch_size=2,
+            on_update_end_fn=_on_update_end,
+        )
+
+    def test_update_frequency_correct_final_iteration(self):
+        trainer = TrainerTrainingLoopMock(100, 2, None, update_frequency=2)
+        trainer.training_loop()
+        self.assertEqual(trainer.max_updates, 2)
+        self.assertEqual(trainer.current_iteration, 4)
+
+    def test_update_frequency_same_model_params(self):
+        trainer1 = self._train_with_condition(
+            num_train_data=100,
+            max_updates=2,
+            max_epochs=None,
+            update_frequency=2,
+            batch_size=2,
+        )
+        trainer2 = self._train_with_condition(
+            num_train_data=100,
+            max_updates=2,
+            max_epochs=None,
+            update_frequency=1,
+            batch_size=4,
+        )
+        self._compare_model_params(trainer1, trainer2)
+
+    def _compare_model_params(self, trainer1, trainer2):
+        for param1, param2 in zip(
+            trainer1.model.parameters(), trainer2.model.parameters()
+        ):
+            self.assertTrue(torch.allclose(param1, param2))
+
+    def _train_with_condition(
+        self,
+        num_train_data,
+        max_updates,
+        max_epochs,
+        update_frequency,
+        batch_size,
+        on_update_end_fn=None,
+    ):
+        torch.random.manual_seed(2)
+        model = SimpleModel(1)
+        opt = torch.optim.SGD(model.parameters(), lr=0.01)
+        trainer = TrainerTrainingLoopMock(
+            num_train_data,
+            max_updates,
+            max_epochs,
+            optimizer=opt,
+            update_frequency=update_frequency,
+            batch_size=batch_size,
+            on_update_end_fn=on_update_end_fn,
+        )
+        model.to(trainer.device)
+        trainer.model = model
+        trainer.training_loop()
+        return trainer
+
     def test_epoch_over_updates(self):
         trainer = TrainerTrainingLoopMock(100, 2, 0.04)
         max_updates = trainer._calculate_max_updates()