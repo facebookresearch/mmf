[fix] fix gradient accumulation when update_frequency != 1 (#747)
Summary:
* fix gradient accumulation when update_frequency is not equal to 1
  * The issue is caused by "start_update" being called at an irregular time so the gradients are actually not being accumulated in between update_frequency. When optimizer's `zero_grad` is called, grad is manually set to zero and therefore the gradients are not being accumulated. This fix made it so that `zero_grad` is called in the correct interval so that grad are being accumulated in between update_frequency.
* fix `combined_report` not taking into account the new losses causing metrics calculation to be incorrect.
* add test (including logging)
  * add test to make sure there is a final update even if the num batches from the current update < update frequency.
* address issue: https://github.com/facebookresearch/mmf/issues/626

Pull Request resolved: https://github.com/facebookresearch/mmf/pull/747

Reviewed By: apsdehal

Differential Revision: D26034213

Pulled By: ytsheng

fbshipit-source-id: fa0c7ad3566ce30f89b67996ac476a3d2dab779a

tests/trainers/test_fp16.py
@@ -30,7 +30,7 @@ class MMFTrainerMock(TrainerTrainingLoopMock, MMFTrainer):
     def __init__(
         self, num_train_data, max_updates, max_epochs, device="cuda", fp16_model=False
     ):
-        super().__init__(num_train_data, max_updates, max_epochs)
+        super().__init__(num_train_data, max_updates, max_epochs, fp16=True)
         self.device = torch.device(device)
         if fp16_model:
             assert (