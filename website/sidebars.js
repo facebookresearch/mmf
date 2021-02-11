95a2225ca7f6116ddb407d49a3fdd2eedea71306
Amanpreet Singh <asg@fb.com>
2021-02-11 14:02:20 -0800

    [docs] Update internal docs to latest
    Summary: Updates fblearner and devserver docs to point to latest content.
    
    Reviewed By: ytsheng
    
    Differential Revision: D25511541
    
    fbshipit-source-id: 58ba9ff19a294c5c419baad50e18d2b3ed1013d7

--- website/sidebars.js
+++ website/sidebars.js
@@ -30,6 +30,7 @@ module.exports = {
     Tutorials: [
       'tutorials/dataset',
       'tutorials/concat_bert_tutorial',
+      'tutorials/image_feature_extraction',
       'tutorials/checkpointing',
       'tutorials/processors',
       'tutorials/slurm',
