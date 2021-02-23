fcba3dffc887bb864f53d6876fdd4618e5af1086
Vedanuj Goswami <vedanuj@fb.com>
2021-02-22 19:05:54 -0800

    [refactor, fix] Fix fbl flow to use iopath, some cleanup
    Summary: Some cleanup of file_io file
    
    Differential Revision: D26599901
    
    fbshipit-source-id: fa2735c9270d9e2ecb6e264023c38c65efce909a

--- mmf/utils/file_io.py
+++ mmf/utils/file_io.py
@@ -1,103 +1,14 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-import os
-import shutil
-from typing import List, Optional
-
-from iopath.common.file_io import PathManager as ioPathManagerClass
+from iopath.common.file_io import PathManager as pm
 
 
-ioPathManager = ioPathManagerClass()
+PathManager = pm()
 
 try:
     # [FB only] register internal file IO handlers
     from mmf.utils.fb.file_io_handlers import register_handlers
 
-    register_handlers(ioPathManager)
+    register_handlers(PathManager)
 except ImportError:
     pass
-
-
-class PathManager:
-    """
-    Wrapper for insulating OSS I/O (using Python builtin operations) from
-    ioPath's PathManager abstraction (for transparently handling various
-    internal backends). This is adapted from
-    `fairseq <https://github.com/pytorch/fairseq/blob/master/fairseq/file_io.py>`.
-    """
-
-    @staticmethod
-    def open(
-        path: str,
-        mode: str = "r",
-        buffering: int = -1,
-        encoding: Optional[str] = None,
-        errors: Optional[str] = None,
-        newline: Optional[str] = None,
-    ):
-        if ioPathManager:
-            return ioPathManager.open(
-                path=path,
-                mode=mode,
-                buffering=buffering,
-                encoding=encoding,
-                errors=errors,
-                newline=newline,
-            )
-        return open(
-            path,
-            mode=mode,
-            buffering=buffering,
-            encoding=encoding,
-            errors=errors,
-            newline=newline,
-        )
-
-    @staticmethod
-    def copy(src_path: str, dst_path: str, overwrite: bool = False) -> bool:
-        if ioPathManager:
-            return ioPathManager.copy(
-                src_path=src_path, dst_path=dst_path, overwrite=overwrite
-            )
-        return shutil.copyfile(src_path, dst_path)
-
-    @staticmethod
-    def get_local_path(path: str, **kwargs) -> str:
-        if ioPathManager:
-            return ioPathManager.get_local_path(path, **kwargs)
-        return path
-
-    @staticmethod
-    def exists(path: str) -> bool:
-        if ioPathManager:
-            return ioPathManager.exists(path)
-        return os.path.exists(path)
-
-    @staticmethod
-    def isfile(path: str) -> bool:
-        if ioPathManager:
-            return ioPathManager.isfile(path)
-        return os.path.isfile(path)
-
-    @staticmethod
-    def ls(path: str) -> List[str]:
-        if ioPathManager:
-            return ioPathManager.ls(path)
-        return os.listdir(path)
-
-    @staticmethod
-    def mkdirs(path: str) -> None:
-        if ioPathManager:
-            return ioPathManager.mkdirs(path)
-        os.makedirs(path, exist_ok=True)
-
-    @staticmethod
-    def rm(path: str) -> None:
-        if ioPathManager:
-            return ioPathManager.rm(path)
-        os.remove(path)
-
-    @staticmethod
-    def register_handler(handler) -> None:
-        if ioPathManager:
-            return ioPathManager.register_handler(handler=handler)
