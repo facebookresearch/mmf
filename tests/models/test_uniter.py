f7fc5903dec1d644fe30fd0aa0a6332291f394f7
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-10-22 14:41:01 -0700

    Update base for Update on "[feat] Add classifier and pretraining UNITER models"
    Add UNITER models to support pretraining heads.
    
    Differential Revision: [D31865996](https://our.internmc.facebook.com/intern/diff/D31865996)
    
    [ghstack-poisoned]

--- tests/models/test_uniter.py
+++ tests/models/test_uniter.py
@@ -3,17 +3,17 @@ import gc
 import unittest
 
 import torch
-from mmf.models.uniter import UniterImageEmbeddings, UniterModelBase
+from mmf.models.uniter import UNITERImageEmbeddings, UNITERModelBase
 from mmf.utils.general import get_current_device
 from omegaconf import OmegaConf
 
 
-class TestUniterImageEmbeddings(unittest.TestCase):
+class TestUNITERImageEmbeddings(unittest.TestCase):
     def test_forward_has_correct_output_dim(self):
         bs = 32
         num_feat = 100
         config = OmegaConf.create({"img_dim": 1024, "hidden_size": 256, "pos_dim": 7})
-        embedding = UniterImageEmbeddings(config)
+        embedding = UNITERImageEmbeddings(config)
         img_feat = torch.rand((bs, num_feat, config["img_dim"]))
         img_pos_feat = torch.rand((bs, num_feat, config["pos_dim"]))
         type_embeddings = torch.ones((bs, num_feat, 1), dtype=torch.long)
@@ -22,7 +22,7 @@ class TestUniterImageEmbeddings(unittest.TestCase):
         self.assertEquals(list(output.shape), [32, 100, 256])
 
 
-class TestUniterModelBase(unittest.TestCase):
+class TestUNITERModelBase(unittest.TestCase):
     def tearDown(self):
         del self.model
         gc.collect()
@@ -30,7 +30,7 @@ class TestUniterModelBase(unittest.TestCase):
     def test_pretrained_model(self):
         img_dim = 1024
         config = OmegaConf.create({"image_embeddings": {"img_dim": img_dim}})
-        self.model = UniterModelBase(config)
+        self.model = UNITERModelBase(config)
 
         self.model.eval()
         self.model = self.model.to(get_current_device())
