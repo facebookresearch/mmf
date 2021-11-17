df69f0908fc172610035fed94b2795f0fa7cbc19
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-11-17 11:27:52 -0800

    Update base for Update on "[feat] Add classifier and pretraining UNITER models"
    Add UNITER models to support pretraining heads.
    
    Differential Revision: [D31865996](https://our.internmc.facebook.com/intern/diff/D31865996)
    
    [ghstack-poisoned]

--- tests/models/test_uniter.py
+++ tests/models/test_uniter.py
@@ -49,6 +49,6 @@ class TestUNITERModelBase(unittest.TestCase):
         with torch.no_grad():
             model_output = model(
                 input_ids, position_ids, img_feat, img_pos_feat, attention_mask
-            )
+            ).final_layer
 
         self.assertEqual(model_output.shape, torch.Size([8, 125, 768]))
