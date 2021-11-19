1af390419feca59c142c56da06a473917c2771a7
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-11-19 15:41:10 -0800

    Add VinVL model wrapper
    Enables VinVL finetuning and pretraining from MMF
    
    [ghstack-poisoned]

--- tests/models/test_vinvl.py
+++ tests/models/test_vinvl.py
@@ -1,14 +1,21 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
+import gc
 import unittest
 
+import tests.test_utils as test_utils
 import torch
+from mmf.common.sample import SampleList
 from mmf.models.vinvl import (
     BertImgModel,
     VinVLForClassification,
     VinVLForPretraining,
 )
+from mmf.utils.build import build_model
+from mmf.utils.configuration import Configuration
+from mmf.utils.env import setup_imports, teardown_imports
 from mmf.utils.general import get_current_device
+from omegaconf import OmegaConf
 from transformers.modeling_bert import BertConfig
 
 
@@ -100,6 +107,116 @@ class TestVinVLForPretraining(unittest.TestCase):
                 token_type_ids_corrupt=token_type_ids,
                 attention_mask_corrupt=attention_mask,
             )
+        self.assertTrue("losses" in model_output)
+        self.assertTrue("masked_lm_loss" in model_output["losses"])
+        self.assertTrue("vinvl_three_way_contrastive" in model_output["losses"])
+
+
+class TestVinVLModel(unittest.TestCase):
+    def setUp(self):
+        test_utils.setup_proxy()
+        setup_imports()
+        model_name = "vinvl"
+        args = test_utils.dummy_args(model=model_name, dataset="test")
+        configuration = Configuration(args)
+        config = configuration.get_config()
+        model_config = config.model_config[model_name]
+        model_config.model = model_name
+        model_config.do_pretraining = False
+        classification_config_dict = {
+            "do_pretraining": False,
+            "heads": {"mlp": {"num_labels": 3129}},
+            "ce_loss": {"ignore_index": -1},
+        }
+        classification_config = OmegaConf.create(
+            {**model_config, **classification_config_dict}
+        )
+
+        pretraining_config_dict = {
+            "do_pretraining": True,
+            "heads": {"mlm": {"hidden_size": 768}},
+        }
+        pretraining_config = OmegaConf.create(
+            {**model_config, **pretraining_config_dict}
+        )
+
+        self.model_for_classification = build_model(classification_config)
+        self.model_for_pretraining = build_model(pretraining_config)
+
+    def tearDown(self):
+        teardown_imports()
+        del self.model_for_classification
+        del self.model_for_pretraining
+        gc.collect()
+
+    def _get_sample_list(self):
+        bs = 8
+        num_feats = 70
+        max_sentence_len = 25
+        img_feature_dim = 2054
+        input_ids = torch.ones((bs, max_sentence_len), dtype=torch.long)
+        img_feats = torch.rand((bs, num_feats, img_feature_dim))
+
+        max_features = torch.ones((bs, num_feats)) * num_feats
+        bbox = torch.randint(50, 200, (bs, num_feats, 4)).float()
+        image_height = torch.randint(100, 300, (bs,))
+        image_width = torch.randint(100, 300, (bs,))
+        image_info = {
+            "max_features": max_features,
+            "bbox": bbox,
+            "image_height": image_height,
+            "image_width": image_width,
+        }
+
+        token_type_ids = torch.zeros_like(input_ids)
+        input_ids_masked = input_ids
+        lm_label_ids = -torch.ones_like(input_ids).long()
+        contrastive_labels = torch.zeros((bs, 1)).long()
+        input_mask = torch.ones((bs, max_sentence_len), dtype=torch.long)
+
+        labels = torch.zeros((bs, 1)).long()
+
+        sample_list = SampleList()
+        sample_list.add_field("input_ids", input_ids)
+        sample_list.add_field("input_ids_corrupt", input_ids)
+        sample_list.add_field("input_ids_masked", input_ids_masked)
+        sample_list.add_field("image_feature_0", img_feats)
+        sample_list.add_field("image_info_0", image_info)
+        sample_list.add_field("input_mask", input_mask)
+        sample_list.add_field("input_mask_corrupt", input_mask)
+        sample_list.add_field("segment_ids", token_type_ids)
+        sample_list.add_field("segment_ids_corrupt", token_type_ids)
+        sample_list.add_field("labels", labels)
+        sample_list.add_field("contrastive_labels", contrastive_labels)
+        sample_list.add_field("lm_label_ids", lm_label_ids)
+        sample_list = sample_list.to(get_current_device())
+        return sample_list
+
+    def test_vinvl_for_classification(self):
+        self.model_for_classification.eval()
+        self.model_for_classification = self.model_for_classification.to(
+            get_current_device()
+        )
+        sample_list = self._get_sample_list()
+
+        sample_list.dataset_name = "test"
+        sample_list.dataset_type = "test"
+        with torch.no_grad():
+            model_output = self.model_for_classification(sample_list)
+
+        self.assertTrue("losses" in model_output)
+        self.assertTrue("ce" in model_output["losses"])
+
+    def test_vinvl_for_pretraining(self):
+        self.model_for_pretraining.eval()
+        self.model_for_pretraining = self.model_for_pretraining.to(get_current_device())
+        sample_list = self._get_sample_list()
+
+        sample_list.dataset_name = "test"
+        sample_list.dataset_type = "test"
+        with torch.no_grad():
+            model_output = self.model_for_pretraining(sample_list)
+
         self.assertTrue("losses" in model_output)
         self.assertTrue("masked_lm_loss" in model_output["losses"])
         self.assertTrue("vinvl_three_way_contrastive" in model_output["losses"])
