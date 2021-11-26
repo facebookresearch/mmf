6fba5f6c07ecfa4fa6a49d0c0c86552d93cefbfc
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-11-26 14:48:08 -0800

    [feat] Add UNITER model (#1115)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/1115
    
    Add nn.Module model from github.com/ChenRocks/UNITER
    modified for mmf. Model is usable without MMF trainer.
    BaseModel class and pretraining to come.
    
    Test Plan:
    **Unit tests**
    Tests instantiation and forward pass of the UNITERModelBase.
    
    Used in UNITER finetuning and pretraining by models in later diffs.
    
    Reviewed By: ebsmothers
    
    Differential Revision: D31768458
    
    Pulled By: Ryan-Qiyu-Jiang
    
    fbshipit-source-id: 499e610085c729e3573c5c12b0a81328580ab19a

--- mmf/models/uniter.py
+++ mmf/models/uniter.py
@@ -3,9 +3,18 @@
 # Initial version was taken from https://github.com/ChenRocks/UNITER/
 # and adapted for MMF.
 
-from typing import Optional
+from collections import namedtuple
+from typing import Optional, Tuple
 
+import torch
+from mmf.utils.general import retry_n
+from omegaconf import DictConfig, OmegaConf
 from torch import Tensor, nn
+from transformers.modeling_bert import BertConfig, BertEmbeddings, BertModel
+
+
+NUM_RETRIES = 6
+EMPTY_CONFIG = OmegaConf.create({})
 
 
 class UNITERImageEmbeddings(nn.Module):
@@ -52,3 +61,161 @@ class UNITERImageEmbeddings(nn.Module):
         embeddings = self.final_layer_norm(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
+
+
+class UNITERModelBase(nn.Module):
+    """UNITER embedding and transformer trunk for V-L modeling.
+    Modified from https://github.com/ChenRocks/UNITER/ for MMF.
+    https://arxiv.org/pdf/1909.11740.pdf
+
+    By default, this model uses the pretrained bert-base-uncased
+    transformer trunk with from huggingface.
+
+    To train on this model through MMF, look at the UNITER model,
+    which supports pretraining and finetuning of UNITERModelBase
+    with configurable heads.
+
+    For an example of using this model standalone,
+    take a look at its unit test in `test_uniter.py`.
+    """
+
+    def __init__(
+        self,
+        random_init: bool = False,
+        bert_model_name: str = "bert-base-uncased",
+        img_dim: int = 2048,
+        hidden_size: int = 768,
+        hidden_dropout_prob: float = 0,
+        text_embeddings: DictConfig = EMPTY_CONFIG,
+        encoder: DictConfig = EMPTY_CONFIG,
+    ):
+        super().__init__()
+
+        bert_config = retry_n(
+            NUM_RETRIES,
+            BertConfig.from_pretrained,
+            bert_model_name,
+            **OmegaConf.to_container(text_embeddings),
+        )
+        self.text_embeddings = BertEmbeddings(bert_config)
+
+        self.img_embeddings = UNITERImageEmbeddings(
+            img_dim=img_dim,
+            hidden_size=hidden_size,
+            hidden_dropout_prob=hidden_dropout_prob,
+        )
+
+        bert_model_name = bert_model_name
+        hf_config = retry_n(
+            NUM_RETRIES,
+            BertConfig.from_pretrained,
+            bert_model_name,
+            **OmegaConf.to_container(encoder),
+        )
+        if random_init:
+            bert_model = BertModel(hf_config)
+        else:
+            bert_model = retry_n(
+                NUM_RETRIES,
+                BertModel.from_pretrained,
+                bert_model_name,
+                config=hf_config,
+            )
+        self.encoder = bert_model.encoder
+        self.pooler = bert_model.pooler
+
+    def _compute_txt_embeddings(
+        self,
+        input_ids: Tensor,
+        position_ids: Tensor,
+        token_type_ids: Optional[Tensor] = None,
+    ) -> Tensor:
+        output = self.text_embeddings(
+            input_ids=input_ids,
+            position_ids=position_ids,
+            token_type_ids=token_type_ids,
+        )
+        return output
+
+    def _compute_img_embeddings(
+        self,
+        img_feat: Tensor,
+        img_pos_feat: Tensor,
+        img_masks: Optional[Tensor] = None,
+        img_type_ids: Optional[Tensor] = None,
+    ) -> Tensor:
+        if img_type_ids is None:
+            img_type_ids = torch.ones_like(img_feat[:, :, 0].long())
+        img_type_embeddings = self.text_embeddings.token_type_embeddings(img_type_ids)
+        output = self.img_embeddings(
+            img_feat, img_pos_feat, img_type_embeddings, img_masks
+        )
+        return output
+
+    def _compute_img_txt_embeddings(
+        self,
+        input_ids: Tensor,
+        position_ids: Tensor,
+        img_feat: Tensor,
+        img_pos_feat: Tensor,
+        img_masks: Optional[Tensor] = None,
+        txt_type_ids: Optional[Tensor] = None,
+        img_type_ids: Optional[Tensor] = None,
+    ) -> Tensor:
+        txt_emb = self._compute_txt_embeddings(input_ids, position_ids, txt_type_ids)
+        img_emb = self._compute_img_embeddings(
+            img_feat, img_pos_feat, img_masks, img_type_ids
+        )
+        embedding_output = torch.cat([txt_emb, img_emb], dim=1)
+        return embedding_output
+
+    def forward(
+        self,
+        input_ids: Tensor,
+        position_ids: Tensor,
+        img_feat: Tensor,
+        img_pos_feat: Tensor,
+        attention_mask: Tensor,
+        img_masks: Optional[Tensor] = None,
+        txt_type_ids: Optional[Tensor] = None,
+        img_type_ids: Optional[Tensor] = None,
+        input_modality: str = "image-text",
+    ) -> Tuple[Tensor, Tensor]:
+        # compute self-attention mask
+        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+        extended_attention_mask = extended_attention_mask.to(
+            dtype=next(self.parameters()).dtype
+        )  # fp16 compatibility
+        # https://github.com/huggingface/transformers/issues/542 for details
+        # on why we add very negative values to attention scores
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        # embedding layer
+        if input_modality == "image":
+            # image only
+            embedding_output = self._compute_img_embeddings(
+                img_feat, img_pos_feat, img_masks, img_type_ids
+            )
+        elif input_modality == "text":
+            # text only
+            embedding_output = self._compute_txt_embeddings(
+                input_ids, position_ids, txt_type_ids
+            )
+        else:
+            embedding_output = self._compute_img_txt_embeddings(
+                input_ids,
+                position_ids,
+                img_feat,
+                img_pos_feat,
+                img_masks,
+                txt_type_ids,
+                img_type_ids,
+            )
+
+        encoded_layers = self.encoder(
+            embedding_output,
+            attention_mask=extended_attention_mask,
+            output_hidden_states=True,
+        )
+        layers = namedtuple("TransformerOutput", ["final_layer", "hidden_layers"])
+        return layers(encoded_layers[0], encoded_layers[1])
--- tests/models/test_uniter.py
+++ tests/models/test_uniter.py
@@ -1,9 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-
 import unittest
 
 import torch
-from mmf.models.uniter import UNITERImageEmbeddings
+from mmf.models.uniter import UNITERImageEmbeddings, UNITERModelBase
+from mmf.utils.general import get_current_device
 from omegaconf import OmegaConf
 
 
@@ -24,3 +24,31 @@ class TestUNITERImageEmbeddings(unittest.TestCase):
             self.img_feat, self.img_pos_feat, self.type_embeddings, img_masks=None
         )
         self.assertEquals(list(output.shape), [32, 100, 256])
+
+
+class TestUNITERModelBase(unittest.TestCase):
+    def test_pretrained_model(self):
+        img_dim = 1024
+        model = UNITERModelBase(img_dim=img_dim)
+
+        model.eval()
+        model = model.to(get_current_device())
+
+        bs = 8
+        num_feats = 100
+        max_sentence_len = 25
+        pos_dim = 7
+        input_ids = torch.ones((bs, max_sentence_len), dtype=torch.long)
+        img_feat = torch.rand((bs, num_feats, img_dim))
+        img_pos_feat = torch.rand((bs, num_feats, pos_dim))
+        position_ids = torch.arange(
+            0, input_ids.size(1), dtype=torch.long, device=img_feat.device
+        ).unsqueeze(0)
+        attention_mask = torch.ones((bs, max_sentence_len + num_feats))
+
+        with torch.no_grad():
+            model_output = model(
+                input_ids, position_ids, img_feat, img_pos_feat, attention_mask
+            ).final_layer
+
+        self.assertEqual(model_output.shape, torch.Size([8, 125, 768]))
