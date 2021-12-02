9723d4afb3b9668aea38794361a211dd9b267823
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-12-02 10:42:31 -0800

    [test] Fix pooler unit tests on gpu
    Fix no attribute "cuda" runtime error when
    testing poolers with cuda.
    Unit tests pass on github actions as
    those machines run without gpu.
    
    [ghstack-poisoned]

--- tests/modules/test_poolers.py
+++ tests/modules/test_poolers.py
@@ -13,36 +13,29 @@ class TestModulePoolers(unittest.TestCase):
         self.num_tokens = 10
         self.embedding_size = 768
         self.token_len = 10
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.encoded_layers = [
-            torch.randn(self.batch_size, self.token_len, self.embedding_size),
-            torch.randn(self.batch_size, self.token_len, self.embedding_size),
-            torch.randn(self.batch_size, self.token_len, self.embedding_size),
+            torch.randn(self.batch_size, self.token_len, self.embedding_size).to(
+                self.device
+            )
+            for _ in range(3)
         ]
-        self.pad_mask = torch.randn(self.batch_size, self.token_len)
+        self.pad_mask = torch.randn(self.batch_size, self.token_len).to(self.device)
 
     def test_AverageConcat(self):
-        pool_fn = poolers.AverageConcatLastN(self.k)
+        pool_fn = poolers.AverageConcatLastN(self.k).to(self.device)
         out = pool_fn(self.encoded_layers, self.pad_mask)
-        if torch.cuda.is_available():
-            pool_fn.cuda()
-            out = pool_fn(self.encoded_layers.cuda(), self.pad_mask.cuda())
 
         assert torch.Size([self.batch_size, self.embedding_size * self.k]) == out.shape
 
     def test_AverageKFromLast(self):
-        pool_fn = poolers.AverageKFromLast(self.k)
+        pool_fn = poolers.AverageKFromLast(self.k).to(self.device)
         out = pool_fn(self.encoded_layers, self.pad_mask)
-        if torch.cuda.is_available():
-            pool_fn.cuda()
-            out = pool_fn([self.encoded_layers.cuda(), self.pad_mask.cuda()])
 
         assert torch.Size([self.batch_size, self.embedding_size]) == out.shape
 
     def test_AverageSumLastK(self):
-        pool_fn = poolers.AverageSumLastK(self.k)
+        pool_fn = poolers.AverageSumLastK(self.k).to(self.device)
         out = pool_fn(self.encoded_layers, self.pad_mask)
-        if torch.cuda.is_available():
-            pool_fn.cuda()
-            out = pool_fn([self.encoded_layers.cuda(), self.pad_mask.cuda()])
 
         assert torch.Size([self.batch_size, self.embedding_size]) == out.shape
