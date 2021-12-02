426de6583a94184a6864db46480ec07eda6643da
ryan-qiyu-jiang <ryan.qiyu.jiang@gmail.com>
2021-12-02 11:14:30 -0800

    [feat] Add classifier and pretraining UNITER models (#1133)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mmf/pull/1133
    
    Add UNITER classification and pretraining models
    that use UNITERBaseModel.
    The pretraining model chooses a task per batch
    from MLM, ITM, MRC, MRFR, and WRA.
    These are nn.Modules usable outside MMF.
    Models forward returns dict with scores and losses.
    For example usage consult the unit tests or UNITER BaseModel.
    
    Test Plan:
    ### Unit Tests
    Tested forward passes for classification and pretraining models.
    
    Pretraining model forward was tested in end-to-end on winoground dataset
    
    Reviewed By: ebsmothers
    
    Differential Revision: D31865996
    
    Pulled By: Ryan-Qiyu-Jiang
    
    fbshipit-source-id: 533e90fe4eec189af55de422f9ae5b2b866f6cac

--- tests/models/test_uniter.py
+++ tests/models/test_uniter.py
@@ -2,7 +2,13 @@
 import unittest
 
 import torch
-from mmf.models.uniter import UNITERImageEmbeddings, UNITERModelBase
+from mmf.common.sample import SampleList
+from mmf.models.uniter import (
+    UNITERForClassification,
+    UNITERForPretraining,
+    UNITERImageEmbeddings,
+    UNITERModelBase,
+)
 from mmf.utils.general import get_current_device
 from omegaconf import OmegaConf
 
@@ -52,3 +58,111 @@ class TestUNITERModelBase(unittest.TestCase):
             ).final_layer
 
         self.assertEqual(model_output.shape, torch.Size([8, 125, 768]))
+
+
+class TestUniterWithHeads(unittest.TestCase):
+    def _get_sample_list(self):
+        bs = 8
+        num_feats = 100
+        max_sentence_len = 25
+        img_dim = 2048
+        cls_dim = 3129
+        input_ids = torch.ones((bs, max_sentence_len), dtype=torch.long)
+        input_mask = torch.ones((bs, max_sentence_len), dtype=torch.long)
+        image_feat = torch.rand((bs, num_feats, img_dim))
+        position_ids = torch.arange(
+            0, max_sentence_len, dtype=torch.long, device=image_feat.device
+        ).unsqueeze(0)
+        img_pos_feat = torch.rand((bs, num_feats, 7))
+        attention_mask = torch.zeros(
+            (bs, max_sentence_len + num_feats), dtype=torch.long
+        )
+        image_mask = torch.zeros((bs, num_feats), dtype=torch.long)
+        targets = torch.rand((bs, cls_dim))
+
+        sample_list = SampleList()
+        sample_list.add_field("input_ids", input_ids)
+        sample_list.add_field("input_mask", input_mask)
+        sample_list.add_field("image_feat", image_feat)
+        sample_list.add_field("img_pos_feat", img_pos_feat)
+        sample_list.add_field("attention_mask", attention_mask)
+        sample_list.add_field("image_mask", image_mask)
+        sample_list.add_field("targets", targets)
+        sample_list.add_field("dataset_name", "test")
+        sample_list.add_field("dataset_type", "test")
+        sample_list["position_ids"] = position_ids
+
+        return sample_list
+
+    def test_uniter_for_classification(self):
+        heads = {"test": {"type": "mlp", "num_labels": 3129}}
+        tasks = "test"
+        losses = {"test": "logit_bce"}
+        model = UNITERForClassification(
+            head_configs=heads, loss_configs=losses, tasks=tasks
+        )
+
+        model.eval()
+        model = model.to(get_current_device())
+        sample_list = self._get_sample_list()
+
+        with torch.no_grad():
+            model_output = model(sample_list)
+
+        self.assertTrue("losses" in model_output)
+        self.assertTrue("test/test/logit_bce" in model_output["losses"])
+
+    def _enhance_sample_list_for_pretraining(self, sample_list):
+        bs = sample_list["input_ids"].size(0)
+        sentence_len = sample_list["input_ids"].size(1)
+
+        is_correct = torch.ones((bs,), dtype=torch.long)
+        lm_label_ids = torch.zeros((bs, sentence_len), dtype=torch.long)
+        input_ids_masked = sample_list["input_ids"]
+        num_feat = sample_list["image_feat"].size(1)
+        cls_dim = 1601
+        image_info = {"cls_prob": torch.rand((bs, num_feat, cls_dim))}
+        sample_list.add_field("is_correct", is_correct)
+        sample_list.add_field("task", "mlm")
+        sample_list.add_field("lm_label_ids", lm_label_ids)
+        sample_list.add_field("input_ids_masked", input_ids_masked)
+        sample_list.add_field("image_info_0", image_info)
+
+    def test_uniter_for_pretraining(self):
+        # UNITER pretraining has 5 pretraining tasks,
+        # we have one unique head for each, and in each
+        # forward pass we train on a different task.
+        # In this test we try running a forward pass
+        # through each head.
+        heads = {
+            "mlm": {"type": "mlm"},
+            "itm": {"type": "itm"},
+            "mrc": {"type": "mrc"},
+            "mrfr": {"type": "mrfr"},
+            "wra": {"type": "wra"},
+        }
+        tasks = "mlm,itm,mrc,mrfr,wra"
+        mask_probability = 0.15
+        model = UNITERForPretraining(
+            head_configs=heads, tasks=tasks, mask_probability=mask_probability
+        )
+        model.eval()
+        model = model.to(get_current_device())
+        sample_list = self._get_sample_list()
+        self._enhance_sample_list_for_pretraining(sample_list)
+
+        expected_loss_names = {
+            "mlm": "masked_lm_loss",
+            "itm": "itm_loss",
+            "mrc": "mrc_loss",
+            "mrfr": "mrfr_loss",
+            "wra": "wra_loss",
+        }
+
+        for task_name, loss_name in expected_loss_names.items():
+            sample_list["task"] = task_name
+            with torch.no_grad():
+                model_output = model(sample_list)
+
+            self.assertTrue("losses" in model_output)
+            self.assertTrue(loss_name in model_output["losses"])
